e587449b23618aa574f6316c9f2a63a08229bc88
Philippe Bruhat (BooK) <book@cpan.org>
2004-03-26 23:37:55 +0000

    Le reste des informations utilisateurs.
    Pour la bio, est-ce qu'il ne vaut pas mieux une table users_bios
    avec conf, user_id, lang, bio? Et pas de champ bio dans la table user?
    (afin que certains puissent avoir une bio multilingue)
    
    D'autres infos utilisateurs dépendent de la conf, j'imagine donc une
    table participations avec:
    
       user_id
       conf
       registered
       paid         /* lié à la base payment */
       payment      /* cash, online, cheque, waived */
       tshirt_size
       nb_family
    
    Ca vous semble cohérent ?
    
    
    git-svn-id: svn://svn.mongueurs.net/act/trunk@69 67b57a05-4208-db11-a765-00306e02d86a

--- database/init_db_Pg.sql
+++ database/init_db_Pg.sql
@@ -5,7 +5,32 @@ CREATE TABLE users
     user_id     serial     NOT NULL  PRIMARY KEY,
     login       text       NOT NULL,
     passwd      text       NOT NULL,
-    session_id  text
+    session_id  text,
+
+    /* personal information */
+    civility     integer,   /* handled in translations */
+    first_name   text,
+    last_name    text,
+    nick_name    text,
+    pseudonymous text,
+    country      text       NOT NULL,
+    town         text,
+
+    /* online indentity */
+    web_page     text,
+    pm_group     text,
+    pm_group_url text,
+    email        text                       NOT NULL,
+    email_hide   boolean      DEFAULT TRUE  NOT NULL,
+    gpg_pub_key  text,
+    pause_id     text,
+    monk_id      text,
+    icq          text,
+    bio          integer,    /* handled in translations? */
+
+    /* website preferences */
+    lang         text         DEFAULT 'Browser select' NOT NULL,
+    timezone     text         DEFAULT 'Europe/Paris'   NOT NULL,
 );
 CREATE INDEX users_session_id ON users (session_id);
 
@@ -32,3 +57,4 @@ CREATE TABLE translations
     text     text
 );
 CREATE INDEX translations_idx ON translations ( tbl, col, id );
+
