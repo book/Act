f80b4febf51facccfe74da7ea5e2d9466b96633f
Éric Cholet <eric@logilune.com>
2004-03-26 19:25:16 +0000

    crée un début de table users pour pouvoir faire l'authentification
    git-svn-id: svn://svn.mongueurs.net/act/trunk@68 67b57a05-4208-db11-a765-00306e02d86a

--- database/init_db_Pg.sql
+++ database/init_db_Pg.sql
@@ -1,11 +1,23 @@
+/* users */
+DROP   TABLE users CASCADE;
+CREATE TABLE users
+(
+    user_id     serial     NOT NULL  PRIMARY KEY,
+    login       text       NOT NULL,
+    passwd      text       NOT NULL,
+    session_id  text
+);
+CREATE INDEX users_session_id ON users (session_id);
+
 /* users' rights */
 DROP   TABLE rights;
 CREATE TABLE rights
 (
     name        text       NOT NULL,
     conf_id     text       NOT NULL,
-    user_id     integer    NOT NULL
+    user_id     integer    NOT NULL,
 
+    FOREIGN KEY( user_id  ) REFERENCES users( user_id  )
 );
 CREATE INDEX rights_idx ON rights (conf_id);
 
@@ -13,11 +25,10 @@ CREATE INDEX rights_idx ON rights (conf_id);
 DROP   TABLE translations;
 CREATE TABLE translations
 (
-    table    text,
+    tbl      text,
     col      text,           
     id       integer,
     lang     text,
-    text     text,
+    text     text
 );
-CREATE INDEX translations_idx ON translations ( table col id );
-
+CREATE INDEX translations_idx ON translations ( tbl, col, id );
