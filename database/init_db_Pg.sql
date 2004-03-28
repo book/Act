99ad260aee84369a2e0fe4e3cefef928ba824711
Philippe Bruhat (BooK) <book@cpan.org>
2004-03-28 22:32:37 +0000

    Une tentative d'ajout des champs pour les talks.
    git-svn-id: svn://svn.mongueurs.net/act/trunk@85 67b57a05-4208-db11-a765-00306e02d86a

--- database/init_db_Pg.sql
+++ database/init_db_Pg.sql
@@ -63,6 +63,40 @@ CREATE TABLE participations
 );
 CREATE INDEX participations_idx ON rights (conf_id);
 
+/*** Talks related tables ***/
+/* talks */
+DROP   TABLE talks;
+CREATE TABLE talks
+(
+    talk_id    integer,
+    conf_id    text,
+    user_id    integer,
+
+    /* talk info */
+    title        text,
+    abstract     text,
+    url_abstract text,
+    url_talk     text,
+    duration     integer NOT NULL,
+    lightning    boolean DEFAULT false NOT NULL,
+
+    /* for the organisers */
+    accepted     boolean DEFAULT false NOT NULL,
+    confirmed    boolean DEFAULT false NOT NULL,
+    comment      text,
+
+    /* for the schedule */
+    room         text,
+    given        timestamp without time zone,
+    /* category_id  integer, */
+  
+
+    FOREIGN KEY( user_id  ) REFERENCES users( user_id )
+    /* FOREIGN KEY( category_id  ) REFERENCES category( category_id ) */
+);
+CREATE INDEX talks_idx ON talks ( conf_id );
+
+
 /* multilingual entries */
 DROP   TABLE translations;
 CREATE TABLE translations
