2a45b09a34783937407709cd89299bdd78bcaf44
Philippe Bruhat (BooK) <book@cpan.org>
2004-04-07 22:54:02 +0000

    La recherche permet d'afficher les utilisateurs sur plusieurs pages. La limite est definie dans [general] searchlimit.
    git-svn-id: svn://svn.mongueurs.net/act/trunk@175 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/User/Search.pm
+++ lib/Act/Handler/User/Search.pm
@@ -7,15 +7,39 @@ use Act::Country;
 
 sub handler {
 
+    # search the users
+    my $offset = $Request{args}{prev}
+               ? $Request{args}{oprev}
+               : $Request{args}{next}
+               ? $Request{args}{onext}
+               : undef;
+    my $limit = $Config->general_searchlimit;
+    my $users = %{$Request{args}}
+              ? Act::User->get_users( %{$Request{args}},
+                  limit => $limit + 1, offset => $offset  )
+              : [];
+
+    # offsets for potential previous/next pages
+    my ($oprev, $onext);
+    $oprev = $offset - $limit if $offset;
+    if (@$users > $limit) {
+       pop @$users;
+       $onext = $offset + $limit;
+    }
+
     # process the search template
     my $template = Act::Template::HTML->new();
     $template->variables(
         countries_iso => \%Act::Country::Countries_by_iso,
-        countries => Act::Country::CountryNames($Request{language}),
-        users => %{$Request{args}}
-               ? Act::User->get_users( %{$Request{args}} ) : [],
+        countries     => Act::Country::CountryNames($Request{language}),
+        users         => $users,
+        oprev         => $oprev,
+        prev          => defined($oprev),   # $oprev can be zero
+        onext         => $onext,
+        next          => defined($onext), 
     );
     $template->process('user/search_form');
+
 }
 
 1;
