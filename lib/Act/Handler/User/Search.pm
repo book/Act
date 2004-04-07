e5a109e3e5a291c1785477aaf0b8671dea242f5f
Philippe Bruhat (BooK) <book@cpan.org>
2004-04-07 13:02:43 +0000

    La recherche d'utilisateurs devrait fonctionner correctement: - /2004/search/ (sans args)   renvoie le moteur de recherche et si on clique sur chercher, on   a la liste complete - pour les utilisateurs, j'ai du ecrire :   [% countries_iso.${u.country}.${global.request.language} %]   pour avoir le nom du pays dans la langue du visiteur   ce qui n'est guere joli
    Reste a faire l'affichage multipages
    
    
    git-svn-id: svn://svn.mongueurs.net/act/trunk@169 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/User/Search.pm
+++ lib/Act/Handler/User/Search.pm
@@ -3,12 +3,15 @@ package Act::Handler::User::Search;
 use Act::Config;
 use Act::Template::HTML;
 use Act::User;
+use Act::Country;
 
 sub search {
 
     # process the search template
     my $template = Act::Template::HTML->new();
     $template->variables(
+        countries_iso => \%Act::Country::Countries_by_iso,
+        countries => Act::Country::CountryNames($Request{language}),
         users => %{$Request{args}}
                ? Act::User->get_users( %{$Request{args}} ) : [],
     );
