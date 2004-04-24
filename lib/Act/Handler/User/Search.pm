b246dc36061488370a4d9b5eeadc64fa7b1c2c4c
Philippe Bruhat (BooK) <book@cpan.org>
2004-04-24 14:54:20 +0000

    Ajoute la liste des groupes de mongueurs à la recherche.
    git-svn-id: svn://svn.mongueurs.net/act/trunk@257 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/User/Search.pm
+++ lib/Act/Handler/User/Search.pm
@@ -31,9 +31,17 @@ sub handler {
     my $countries = Act::Country::CountryNames();
     my %by_iso = map { $_->{iso} => $_->{name} } @$countries;
 
+    # fetch the monger groups
+    my $SQL = 'SELECT u.pm_group FROM users u, participations p WHERE u.user_id=p.user_id AND p.conf_id=? AND u.pm_group IS NOT NULL';
+    my $sth = $Request{dbh}->prepare_cached( $SQL );
+    $sth->execute( $Request{conference} );
+    $pm_groups = [ map { ucfirst } $sth->fetchrow_array() ];
+    $sth->finish;
+
     # process the search template
     my $template = Act::Template::HTML->new();
     $template->variables(
+        pm_groups     => $pm_groups,
         countries_iso => \%by_iso,
         countries     => $countries,
         users         => $users,
