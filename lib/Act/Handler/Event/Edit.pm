25f0346c83077b4e74926c6ee1f00cd6546b5944
Philippe Bruhat (BooK) <book@cpan.org>
2004-05-19 15:56:50 +0000

    Edition de date pour les événements
    git-svn-id: svn://svn.mongueurs.net/act/trunk@419 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/Event/Edit.pm
+++ lib/Act/Handler/Event/Edit.pm
@@ -14,10 +14,12 @@ use Act::Template;
 # form
 my $form = Act::Form->new(
   required => [qw( title abstract )],
-  optional => [qw( url_abstract duration datetime )],
+  optional => [qw( url_abstract duration date time room )],
   constraints => {
      duration     => sub { $_[0] =~ /^\d+$/ },
      url_abstract => 'url',
+     date         => 'date',
+     time         => 'time',
   }
 );
 
@@ -29,6 +31,12 @@ sub handler {
     }
     my $template = Act::Template::HTML->new();
     my $fields;
+    my $sdate = DateTime::Format::Pg->parse_timestamp($Config->talks_start_date)
+;
+    my $edate = DateTime::Format::Pg->parse_timestamp($Config->talks_end_date);
+    my @dates = ($sdate->clone->truncate(to => 'day' ));
+    push @dates, $_
+        while (($_ = $dates[-1]->clone->add( days => 1 ) ) < $edate );
 
     # get the event
     my $event;
@@ -50,6 +58,19 @@ sub handler {
         # validate form fields
         my $ok = $form->validate($Request{args});
         $fields = $form->{fields};
+
+        # is the date in range?
+        unless ( not exists $fields->{date}
+              or not exists $fields->{time}
+              or exists $form->{invalid}{date}
+              or exists $form->{invalid}{time} ) {
+            $fields->{datetime} = DateTime::Format::Pg->parse_timestamp("$fields->{date} $fields->{time}:00");
+            if ( $fields->{datetime} > $edate or
+                 $fields->{datetime} < $sdate ) {
+                $form->{invalid}{period} = 'invalid';
+                $ok = 0;
+            }
+        }
 
         if ($ok) {
             # update existing event
@@ -76,12 +97,15 @@ sub handler {
             $form->{invalid}{abstract}     && push @errors, 'ERR_ABSTRACT';
             $form->{invalid}{duration}     && push @errors, 'ERR_DURATION';
             $form->{invalid}{url_abstract} && push @errors, 'ERR_URL_ABSTRACT';
+            $form->{invalid}{date}         && push @errors, 'ERR_DATE';
+            $form->{invalid}{time}         && push @errors, 'ERR_TIME';
+            $form->{invalid}{period}       && push @errors, 'ERR_DATERANGE';
         }
         $template->variables(errors => \@errors);
     }
 
     # display the event submission form
-    $template->variables( defined $event ? ( %$event ) : ( %$fields ) );
+    $template->variables( dates => \@dates, defined $event ? ( %$event ) : ( %$fields ) );
     $template->process('event/add');
 }
 
