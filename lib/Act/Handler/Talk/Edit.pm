3c776f123f5b296b9b5f5b731fa12f74709c357c
Éric Cholet <eric@logilune.com>
2005-02-06 23:56:04 +0000

    tentative de correction du bug de newtalk
    git-svn-id: svn://svn.mongueurs.net/act/trunk@609 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/Talk/Edit.pm
+++ lib/Act/Handler/Talk/Edit.pm
@@ -68,7 +68,7 @@ sub handler {
 
         # validate form fields
         my $ok = $form->validate($Request{args});
-        $fields = { accepted => 0, confirmed => 0, %{$form->{fields}} };
+        $fields = { %{$form->{fields}}, accepted => 0, confirmed => 0 };
 
         # organizer specifies user id
         my $user_id = $Request{user}->is_orga
