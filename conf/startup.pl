ce7de850338baedab536b3b0ee9f1155d97ab37e
Éric Cholet <eric@logilune.com>
2005-04-18 17:49:33 +0000

    meilleur preloading mod_perl
    git-svn-id: svn://svn.mongueurs.net/act/trunk@691 67b57a05-4208-db11-a765-00306e02d86a

--- conf/startup.pl
+++ conf/startup.pl
@@ -8,30 +8,47 @@ use Apache::Constants  qw(:common);
 # CPAN modules
 use Apache::AuthCookie ();
 use Apache::Cookie     ();
+use Apache::Request    ();
 use AppConfig          ();
+use DateTime           ();
 use DBI                ();
 use Digest::MD5        ();
 use Email::Valid       ();
 use HTML::Entities     ();
 use MIME::Lite         ();
 use Net::SMTP          ();
-use Storable           ();
 use Template           ();
 use URI::Escape        ();
 
-# Act modules
+# Act handlers
 use Act::Auth;
-use Act::Config;
 use Act::Dispatcher;
-use Act::Email;
-use Act::Form;
+use Act::Handler::Event::Edit;
+use Act::Handler::Event::Show;
+use Act::Handler::Login;
+use Act::Handler::Logout;
 use Act::Handler::Payment::Confirm;
-use Act::Talk;
-use Act::Template;
-use Act::User;
-use Act::Util;
-
+use Act::Handler::Payment::Invoice;
+use Act::Handler::Payment::List;
+use Act::Handler::Payment::Payment;
 use Act::Handler::Static;
+use Act::Handler::Talk::Edit;
+use Act::Handler::Talk::Export;
+use Act::Handler::Talk::Import;
+use Act::Handler::Talk::List;
+use Act::Handler::Talk::Schedule;
+use Act::Handler::Talk::Show;
+use Act::Handler::User::Change;
+use Act::Handler::User::Main;
+use Act::Handler::User::Photo;
+use Act::Handler::User::Purchase;
+use Act::Handler::User::Register;
+use Act::Handler::User::ResetPassword;
+use Act::Handler::User::Rights;
+use Act::Handler::User::Search;
+use Act::Handler::User::Show;
+use Act::Handler::User::Stats;
+use Act::Handler::User::Unregister;
 
 # preload DBD drivers
 DBI->install_driver('Pg');
