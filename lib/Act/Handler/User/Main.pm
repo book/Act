9191ed319cc3a157390b91d628c357ba3be0d191
Philippe Bruhat (BooK) <book@cpan.org>
2006-03-03 16:25:38 +0000

    correctly sort the previous Act conferences
    git-svn-id: svn://svn.mongueurs.net/act/trunk@859 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/User/Main.pm
+++ lib/Act/Handler/User/Main.pm
@@ -54,7 +54,7 @@ sub handler {
     }
     $template->variables(
         talks => $talks,
-        conferences => [ sort { $b->{start} cmp $a->{start} } values %confs ],
+        conferences => [ sort { $b->{begin} cmp $a->{begin} } values %confs ],
         can_unregister =>  $Request{user}->has_registered()
                        && !$Request{user}->has_paid()
                        && !$Request{user}->has_talk(),
