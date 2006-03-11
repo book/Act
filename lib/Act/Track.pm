28dc54a42212e3987791e041628e5181180ecdfc
Philippe Bruhat (BooK) <book@cpan.org>
2006-03-11 22:42:08 +0000

    les objets Track
    git-svn-id: svn://svn.mongueurs.net/act/trunk@905 67b57a05-4208-db11-a765-00306e02d86a

---
+++ lib/Act/Track.pm
@@ -0,0 +1,35 @@
+package Act::Track;
+use Act::Object;
+use base qw( Act::Object );
+
+# class data used by Act::Object
+our $table       = 'tracks';
+our $primary_key = 'track_id';
+
+our %sql_stub    = (
+    select => "t.*",
+    from   => "tracks t",
+);
+our %sql_mapping = (
+    title        => "(t.title~*?)",
+    description  => "(t.description~*?)",
+    # standard stuff
+    map( { ($_, "(t.$_=?)") }
+         qw( track_id conf_id ) )
+);
+our %sql_opts    = ( 'order by' => 'track_id' );
+
+*get_tracks = \&Act::Object::get_items;
+
+=head1 NAME
+
+Act::Track - An Act object representing a talk track.
+
+=head1 DESCRIPTION
+
+This is a standard Act::Object class. See Act::Object for details.
+
+=cut
+
+1;
+
