f578b848b4e334904d8002277225494714c00d21
Philippe Bruhat (BooK) <book@cpan.org>
2006-03-12 16:09:12 +0000

    Add support for the tracks in the talks and schedule
    Move the talks table in a sub-template
    
    
    git-svn-id: svn://svn.mongueurs.net/act/trunk@907 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/Talk/List.pm
+++ lib/Act/Handler/Talk/List.pm
@@ -4,12 +4,23 @@ use Apache::Constants qw(NOT_FOUND);
 use Act::Config;
 use Act::Template::HTML;
 use Act::Talk;
+use Act::Track;
 
 sub handler
 {
     # retrieve talks and speaker info
-    my $talks = Act::Talk->get_talks(conf_id => $Request{conference});
-    $_->{user} = Act::User->new(user_id => $_->user_id) for @$talks;
+    my $talks =Act::Talk->get_talks( conf_id => $Request{conference} );
+    $_->{user} = Act::User->new( user_id => $_->user_id ) for @$talks;
+
+    # sort talks
+    $talks = [
+        sort {
+                   $a->lightning <=> $b->lightning
+                || lc $a->{user}->last_name cmp lc $b->{user}->last_name
+                || lc $a->{user}->first_name cmp lc $b->{user}->first_name
+                || $a->talk_id <=> $b->talk_id
+        } @$talks
+    ];
 
     # accept / unaccept talks
     if ($Request{user} && $Request{user}->is_orga && $Request{args}{ok}) {
@@ -28,19 +39,22 @@ sub handler
     my ($accepted, $lightnings, $duration ) = ( 0, 0, 0 );
     $_->accepted && do { $accepted++; $_->lightning ? $lightnings++ : ( $duration += $_->duration) } for @$talks;
 
+    # link the talks to their tracks (keeping the talks ordered)
+    my $tracks = Act::Track->get_tracks( conf_id => $Request{conference} );
+    for my $track ( @$tracks ) {
+        my $id = $track->track_id;
+        $track->{talks} = [ grep { $_->track_id eq $id } @talks ];
+    }
+    
     # process the template
     my $template = Act::Template::HTML->new();
     $template->variables(
-        talks => [ sort {
-            $a->lightning <=> $b->lightning
-            || lc $a->{user}->last_name  cmp lc $b->{user}->last_name
-            || lc $a->{user}->first_name cmp lc $b->{user}->first_name
-            || $a->talk_id <=> $b->talk_id
-        } @$talks ],
+        talks          => $talks,
         talks_total    => scalar @$talks,
         talks_accepted => $accepted,
         talks_duration => $duration,
         talks_lightning => $lightnings,
+        tracks         => $tracks,
     ); 
     $template->process('talk/list');
 }
