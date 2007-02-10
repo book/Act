017be609caf1df8786ff85f72465c1c7a0b64e1c
Éric Cholet <eric@logilune.com>
2007-02-10 19:02:18 +0000

    top ten countries of already registered users: also in orga "create user" page
    git-svn-id: svn://svn.mongueurs.net/act/trunk@1140 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Country.pm
+++ lib/Act/Country.pm
@@ -1,5 +1,6 @@
 package Act::Country;
 use strict;
+use Act::Config;
 use Act::Util;
 
 sub CountryNames
@@ -19,6 +20,23 @@ sub CountryName
    return Act::Util::get_translation('countries', 'iso', $code) || $code;
 }
 
+sub TopTen
+{
+    # top 10 countries of registered users
+    my $sth = $Request{dbh}->prepare_cached(
+        'SELECT u.country FROM users u, PARTICIPATIONS p'
+      . ' WHERE u.user_id = p.user_id AND p.conf_id = ?'
+      . ' GROUP BY u.country ORDER BY COUNT(u.country) DESC LIMIT 10'
+      );
+      $sth->execute( $Request{conference} );
+      my @topten = map {{ iso  => $_->[0],
+                          name => CountryName($_->[0]),
+                       }}
+                       @{ $sth->fetchall_arrayref([]) };
+      $sth->finish;
+      return \@topten;
+}
+
 1;
 
 __END__
@@ -31,6 +49,7 @@ Act::Country - get country information
 
     use Act::Country;
     my $countries = Act::Country::CountryNames;
+    my $topten = Act::Country::TopTen;
     my $country_name = Act::Country::CountryName($iso_code);
 
 =cut
