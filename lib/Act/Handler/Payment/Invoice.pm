b63deba4fde7bad4adc49bcaef1e20d9bda3885c
Éric Cholet <eric@logilune.com>
2007-05-27 17:21:08 +0000

    invoice: user info (name, address) is now a form, instead of a link to user/change let treasurer generate invoices
    git-svn-id: svn://svn.mongueurs.net/act/trunk@1430 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/Payment/Invoice.pm
+++ lib/Act/Handler/Payment/Invoice.pm
@@ -2,17 +2,24 @@ package Act::Handler::Payment::Invoice;
 use strict;
 
 use Apache::Constants qw(NOT_FOUND FORBIDDEN);
-use Act::Template::HTML;
+
 use Act::Config;
+use Act::Form;
 use Act::User;
 use Act::Invoice;
 use Act::Order;
+use Act::Template::HTML;
 use Act::Util;
+
+my $form = Act::Form->new(
+  required => [qw(first_name last_name address)],
+  optional => [qw(company vat)],
+);
 
 sub handler
 {
     # invoices must be enabled
-    unless ($Config->payment_invoices) {
+    unless ($Config->payment_invoices || $Request{user}->is_treasurer) {
         $Request{status} = NOT_FOUND;
         return;
     }
@@ -26,11 +33,13 @@ sub handler
 
     # get the order
     my $order   = Act::Order->new(   order_id => $order_id );
+    unless ($order) {
+        $Request{status} = NOT_FOUND;
+        return;
+    }
 
     # only a treasurer or the client can see the invoice
-    unless ( $order
-        &&  ($Request{user}->user_id == $order->user_id || $Request{user}->is_treasurer))
-    {
+    unless ($Request{user}->user_id == $order->user_id || $Request{user}->is_treasurer) {
         $Request{status} = FORBIDDEN;
         return;
     }
@@ -41,22 +50,49 @@ sub handler
 
     # invoice doesn't exist
     unless ($invoice) {
+        my $fields;
+        my $user = Act::User->new(user_id => $order->user_id);
+
         # user confirms billing info
         if ($Request{args}{ok}) {
-            # create invoice
-            $invoice = Act::Invoice->create(
-                (map { $_ => $order->$_ }         qw(order_id amount currency means)),
-                (map { $_ => $Request{user}->$_ } qw( first_name last_name company vat address )),
-            );
-            # redirect to canonical invoice URL
-            return Act::Util::redirect(make_uri_info('invoice', $order_id));
+            # form has been submitted
+            my @errors;
+    
+            # validate form fields
+            my $ok = $form->validate($Request{args});
+            $fields = $form->{fields};
+
+            if ($ok) {
+                # update user
+                $user->update(%$fields);
+
+                # create invoice
+                $invoice = Act::Invoice->create(
+                    (map { $_ => $order->$_ } qw(order_id amount currency means)),
+                    (map { $_ => $user->$_ }  qw(first_name last_name company vat address)),
+                );
+                # redirect to canonical invoice URL
+                return Act::Util::redirect(make_uri_info('invoice', $order_id));
+            }
+            else {
+                # map errors
+                $form->{invalid}{first_name} && push @errors, 'ERR_FIRST_NAME';
+                $form->{invalid}{last_name}  && push @errors, 'ERR_LAST_NAME';
+                $form->{invalid}{address}    && push @errors, 'ERR_ADDRESS';
+            }
+            $template->variables(errors => \@errors);
         }
         else {
-            # display billing info confirmation form
-            $template->variables(order_id => $order_id);
-            $template->process('payment/confirm_invoice');
-            return;
+            $fields = {%{$user}};
+            $user->address or $template->variables(errors => [ 'ERR_ADDRESS' ]);
         }
+        # display billing info confirmation form
+        $template->variables(
+            order_id => $order_id,
+            %$fields,
+        );
+        $template->process('payment/confirm_invoice');
+        return;
     }
 
     # process the template
