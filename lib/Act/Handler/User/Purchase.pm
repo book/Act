1ec3c8cba92814696b654a728e8e469d99c61073
Éric Cholet <eric@logilune.com>
2007-05-27 18:45:08 +0000

    allow donation in user payment
    git-svn-id: svn://svn.mongueurs.net/act/trunk@1431 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/User/Purchase.pm
+++ lib/Act/Handler/User/Purchase.pm
@@ -2,10 +2,19 @@ package Act::Handler::User::Purchase;
 use Apache::Constants qw(NOT_FOUND);
 
 use Act::Config;
+use Act::Form;
 use Act::Order;
 use Act::Payment;
 use Act::Template::HTML;
 
+my $form = Act::Form->new(
+    required => [qw(price)],
+    optional => [qw(donation)],
+    constraints => {
+        price    => sub { Act::Payment::get_price($_[0]) },
+        donation => 'numeric',
+    }
+);
 sub handler
 {
     # shouldn't get here unless the online payment is open
@@ -21,39 +30,43 @@ sub handler
     $Request{r}->no_cache(1);
     my $template = Act::Template::HTML->new();
 
-    if (   $Request{args}{purchase}
-        && $Request{args}{price}
-        && (my $price = Act::Payment::get_price($Request{args}{price})))
-    {
-        # first form has been submitted
-        # purge existing order from previous failed attempt
-        # and always a use a newly created order
-        # (the bank will only process an order once)
-        my %f = (
-            user_id  => $Request{user}{user_id},
-            conf_id  => $Request{conference},
-            amount   => $price->{amount},
-            currency => $Config->payment_currency,
-            status   => 'init',
-        );
-        my $order = Act::Order->new(%f);
-        $order->delete() if $order;
-        $order = Act::Order->create(%f);
-
-        # display second form (submits to the bank)
-        my $plugin = Act::Payment::load_plugin();
-        $template->variables(order => $order);
-        $template->variables_raw(form => $plugin->create_form($order));
-        $template->process('user/payment');
+    if ($Request{args}{purchase}) {
+        # form has been submitted
+        # validate form fields
+        my $ok = $form->validate($Request{args});
+        my $fields = $form->{fields};
+        if ($ok) {
+            # first form has been submitted
+            # purge existing order from previous failed attempt
+            # and always a use a newly created order
+            # (some banks will only process a given order_id once)
+            my $amount = Act::Payment::get_price($fields->{price})->{amount}
+                       + $fields->{donation};
+            my %f = (
+                user_id  => $Request{user}{user_id},
+                conf_id  => $Request{conference},
+                amount   => $amount,
+                currency => $Config->payment_currency,
+                status   => 'init',
+            );
+            my $order = Act::Order->new(%f);
+            $order->delete() if $order;
+            $order = Act::Order->create(%f);
+    
+            # display second form (submits to the bank)
+            my $plugin = Act::Payment::load_plugin();
+            $template->variables(order => $order);
+            $template->variables_raw(form => $plugin->create_form($order));
+            $template->process('user/payment');
+            return;
+        }
     }
-    else {
-        # display the first form
-        $template->variables(
-            currency => $Config->payment_currency,
-            prices   => Act::Payment::get_prices,
-        );
-        $template->process('user/purchase');
-    }
+    # display the first form
+    $template->variables(
+        currency => $Config->payment_currency,
+        prices   => Act::Payment::get_prices,
+    );
+    $template->process('user/purchase');
 }
 
 1;
