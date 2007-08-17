0969b0f00b2f1d6d129541a5d9c945ed82826369
Éric Cholet <eric@logilune.com>
2007-08-17 09:01:30 +0000

    Merged revisions 1600,1602-1605 via svnmerge from svn://svn.mongueurs.net/act/trunk
    ........
      r1600 | eric | 2007-08-14 13:37:08 +0200 (Tue, 14 Aug 2007) | 2 lines
    
      to do
    ........
      r1603 | eric | 2007-08-15 21:07:47 +0200 (Wed, 15 Aug 2007) | 3 lines
    
      treasurer can edit payments that he's created
      and with no associated invoice
    ........
      r1604 | eric | 2007-08-15 22:49:43 +0200 (Wed, 15 Aug 2007) | 1 line
    
      localize new string
    ........
      r1605 | eric | 2007-08-16 14:54:46 +0200 (Thu, 16 Aug 2007) | 1 line
    
      payments list: sortable headers
    ........
    
    
    git-svn-id: svn://svn.mongueurs.net/act/branches/stable@1613 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/Payment/List.pm
+++ lib/Act/Handler/Payment/List.pm
@@ -1,12 +1,70 @@
 package Act::Handler::Payment::List;
 use strict;
 use Apache::Constants qw(NOT_FOUND);
+use DateTime;
+
 use Act::Config;
 use Act::Invoice;
 use Act::Order;
+use Act::Payment;
 use Act::Template::HTML;
 use Act::User;
 use Act::Util;
+
+*_n = \&Act::Util::normalize;
+
+my %sortsub = (
+    user    => sub { [ Act::Util::usort { $_->last_name } @{$_[0]} ] },
+    status  => sub { my ($users, $orders) = @_;
+                return [ sort {
+                    # sort users with rights first
+                    my ($sa, $sb) = map $_->{status}, $a, $b;
+                      @$sa && @$sb ? join('', @$sa) cmp join('', @$sb)
+                                                    ||
+                                     _n($a->last_name) cmp _n($b->last_name)
+                    : @$sa         ? -1
+                    : @$sb         ? 1
+                    :                _n($a->last_name) cmp _n($b->last_name)
+                } @$users ]
+        
+
+               },
+    date    => sub { my ($users, $orders) = @_;
+                return [ sort {
+                    # sort users with payments first
+                    my ($oa, $ob) = map $orders->{$_->user_id}, $a, $b;
+                      $oa && $ob ? DateTime->compare($oa->datetime, $ob->datetime)
+                    : $oa        ? -1
+                    : $ob        ? 1
+                    :              _n($a->last_name) cmp _n($b->last_name)
+                } @$users ]
+               },
+    means   => sub { my ($users, $orders) = @_;
+                return [ sort {
+                    # sort users with payments first
+                    my ($oa, $ob) = map $orders->{$_->user_id}, $a, $b;
+                      $oa && $ob ? $oa->means cmp $ob->means
+                    : $oa        ? -1
+                    : $ob        ? 1
+                    :              _n($a->last_name) cmp _n($b->last_name)
+                } @$users ]
+             },
+    price   => sub {
+                my ($users, $orders) = @_;
+                return [ sort {
+                    # sort users with payments first
+                    my ($oa, $ob) = map $orders->{$_->user_id}, $a, $b;
+                      $oa && $ob ? $oa->amount <=> $ob->amount
+                                               ||
+                                   _n($a->last_name) cmp _n($b->last_name)
+                    : $oa        ? -1
+                    : $ob        ? 1
+                    :              _n($a->last_name) cmp _n($b->last_name)
+                } @$users ]
+             },
+);
+# hack to let TT access 'status'
+sub Act::User::status { $_[0]->{status} }
 
 sub handler
 {
@@ -17,28 +75,43 @@ sub handler
     }
     # retrieve users and their payment info
     my $users = Act::User->get_items( conf_id => $Request{conference} );
+    my $means  = Act::Payment::get_means;
     my (%orders, %invoice_uri, %total);
     for my $u (@$users) {
-        $orders{$u->user_id} = Act::Order->new(
+        $u->{status} = [ keys %{$u->rights}, $u->has_talk ? 'speaker' : () ];
+
+        my $order = Act::Order->new(
             user_id  => $u->user_id,
             conf_id  => $Request{conference},
             status   => 'paid',
         );
-        if ($orders{$u->user_id}) {
-            $orders{$u->user_id}{means} = localize('payment_means_' . $orders{$u->user_id}{means});
-            $invoice_uri{$u->user_id} = make_uri_info('invoice', $orders{$u->user_id}->order_id);
-            $total{ $orders{$u->user_id}{currency} } += $orders{$u->user_id}{amount};
+        if ($order) {
+            my $invoice = Act::Invoice->new( order_id => $order->order_id );
+            if ($invoice) {
+                $order->{invoice_no} = $invoice->invoice_no;
+            }
+            else {
+                # editable if created by treasurer, no invoice, same currency
+                $order->{editable} = exists $means->{$order->means}
+                                    && $order->currency eq $Config->payment_currency;
+            }
+            $order->{means} = localize('payment_means_' . $order->means);
+            $invoice_uri{$u->user_id} = make_uri_info('invoice', $order->order_id);
+            $total{ $order->currency } += $order->amount;
+            $orders{$u->user_id} = $order;
         }
     }
+    # sort key
+    my $sortkey = $Request{args}{sort};
+    $sortkey = 'user' unless $sortkey && exists $sortsub{$sortkey};
 
     # process the template
     my $template = Act::Template::HTML->new();
     $template->variables(
-        users => [ Act::Util::usort { $_->last_name }
-                   @$users
-                 ],
-        orders => \%orders,
-        total  => \%total,
+        users       => $sortsub{$sortkey}->($users, \%orders),
+        sortkey     => $sortkey,
+        orders      => \%orders,
+        total       => \%total,
         invoice_uri => \%invoice_uri,
     ); 
     $template->process('payment/list');
--- lib/Act/Handler/Payment/Payment.pm
+++ lib/Act/Handler/Payment/Payment.pm
@@ -2,6 +2,7 @@ package Act::Handler::Payment::Payment;
 use strict;
 use Apache::Constants qw(NOT_FOUND);
 use Act::Config;
+use Act::Invoice;
 use Act::Order;
 use Act::Payment;
 use Act::Template::HTML;
@@ -10,61 +11,87 @@ use Act::Util;
 
 sub handler
 {
+    _handler() or $Request{status} = NOT_FOUND;
+}
+sub _handler
+{
     # for treasurers only
-    if ($Request{user} && $Request{user}->is_treasurer) {
-        if ($Request{args}{user_id}) {
-            # fetch user
-            my $user = Act::User->new(
-                user_id => $Request{args}{user_id},
-                conf_id => $Request{conference},
-            );
-            unless ($user->has_paid) {
-                # payment means and prices
-                my $means = Act::Payment::get_means();
-                delete $means->{ONLINE};
-                my $prices = Act::Payment::get_prices;
+    return unless $Request{user}->is_treasurer && $Request{args}{user_id};
 
-                if ($Request{args}{ok}) {
-                    # payment form submission
-                    my %prices = map { $_->{price_id} => $_ } @$prices;
-                    my ($amount, $price);
-                    if ($Request{args}{means} eq 'FREE') {
-                        $amount = 0;
-                    }
-                    elsif ($Request{args}{amount}) {
-                        $amount = $Request{args}{amount};
-                    }
-                    else {
-                        $amount = $prices{$Request{args}{price}}{amount};
-                        $price  = $prices{$Request{args}{price}}{name};
-                    }
-                    # create an order row for this payment
-                    Act::Order->create(
-                        user_id  => $user->user_id,
-                        conf_id  => $Request{conference},
-                        amount   => $amount,
-                        currency => $Config->payment_currency,
-                        means    => $Request{args}{means},
-                        price    => $price,
-                        status   => 'paid',
-                    );
-                    # back to the payment list
-                    return Act::Util::redirect(make_uri('payments'));
-                }
-                # display payment form
-                my $template = Act::Template::HTML->new;
-                $template->variables(
-                    user   => $user,
-                    means  => $means,
-                    prices => $prices,
-                    currency => $Config->payment_currency,
-                );
-                $template->process('payment/payment');
-                return;
-            }
-        }
+    # payment means and prices
+    my $means  = Act::Payment::get_means;
+    my $prices = Act::Payment::get_prices;
+
+    # fetch user
+    my $user = Act::User->new(
+        user_id => $Request{args}{user_id},
+        conf_id => $Request{conference},
+    );
+    return unless $user;
+
+    # fetch order
+    my $order = Act::Order->new(
+            user_id  => $Request{args}{user_id},
+            conf_id  => $Request{conference},
+            status   => 'paid',
+    );
+    # can create payment,
+    if ($order) {
+        # edit existing except online
+        return unless exists $means->{$order->means};
+        # or if invoice already generated
+        my $invoice = Act::Invoice->new( order_id => $order->order_id );
+        return if $invoice;
     }
-    $Request{status} = NOT_FOUND;
+    if ($Request{args}{ok}) {
+        # payment form submission
+        return if $Request{args}{order_id} && $Request{args}{order_id} != $order->order_id;
+        my %prices = map { $_->{price_id} => $_ } @$prices;
+        my ($amount, $price);
+        if ($Request{args}{means} eq 'FREE') {
+            $amount = 0;
+        }
+        elsif ($Request{args}{amount}) {
+            $amount = $Request{args}{amount};
+        }
+        else {
+            $amount = $prices{$Request{args}{price}}{amount};
+            $price  = $prices{$Request{args}{price}}{name};
+        }
+        # create or update the order
+        if ($order) {
+            $order->update(
+                means    => $Request{args}{means},
+                price    => $price,
+                amount   => $amount,
+            )
+        }
+        else {
+            Act::Order->create(
+                user_id  => $user->user_id,
+                conf_id  => $Request{conference},
+                means    => $Request{args}{means},
+                price    => $price,
+                amount   => $amount,
+                currency => $Config->payment_currency,
+                status   => 'paid',
+            );
+        }
+        # back to the payment list
+        Act::Util::redirect(make_uri('payments'));
+        return 1;
+    }
+    # display payment form
+    my $template = Act::Template::HTML->new;
+    $template->variables( %$order ) if $order;
+    $template->variables(
+        user      => $user,
+        allmeans  => $means,
+        prices    => $prices,
+        currency  => $Config->payment_currency,
+    );
+    $template->process('payment/payment');
+    return 1;
 }
 
 1;
