5afd9484728526db58795e038981f98610249e55
Éric Cholet <eric@logilune.com>
2007-09-11 18:42:41 +0000

    news - remove duplicate database fetches
    git-svn-id: svn://svn.mongueurs.net/act/trunk@1680 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/News/Fetch.pm
+++ lib/Act/Handler/News/Fetch.pm
@@ -25,9 +25,10 @@ sub fetch
     # apply optional limit
     $#$news = $count - 1 if $count && @$news > $count;
 
+    my %users;
     for my $n (@$news) {
         # fetch user
-        $n->{user} = Act::User->new(user_id => $n->user_id);
+        $n->{user} = $users{$n->user_id} ||= Act::User->new(user_id => $n->user_id);
         # fetch title and text
         my $item = $n->items->{$Request{language}};
         $n->{$_} = $item->{$_} for qw(title text);
