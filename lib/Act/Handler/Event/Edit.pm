915b8f0a2710ab0e4039546e6cb122cb631e68fa
Éric Cholet <eric@logilune.com>
2008-02-05 18:06:22 +0000

    Merged revisions 1761,1766-1774 via svnmerge from svn://svn.mongueurs.net/act/trunk
    ........
      r1761 | book | 2008-02-05 09:31:45 +0100 (Mar, 05 fév 2008) | 1 line
    
      return to the schedule after editing an event from the schedule
    ........
      r1773 | book | 2008-02-05 18:48:44 +0100 (Mar, 05 fév 2008) | 1 line
    
      add a return_url hidden field
    ........
      r1774 | book | 2008-02-05 18:59:06 +0100 (Mar, 05 fév 2008) | 1 line
    
      don't forget the return_url variable
    ........
    
    
    git-svn-id: svn://svn.mongueurs.net/act/branches/stable@1779 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/Event/Edit.pm
+++ lib/Act/Handler/Event/Edit.pm
@@ -51,6 +51,11 @@ sub handler {
         return;
     }
 
+    # automatically compute the return URL
+    my $referer = $Request{r}->header_in('Referer');
+    $Request{args}{return_url} ||= $referer
+        if $referer =~ m{/(?:schedule)};
+
     if ($Request{args}{submit}) {
         # form has been submitted
         my @errors;
@@ -98,6 +103,10 @@ sub handler {
 
                 return;
             }
+
+            # return to the referring URL if needed
+            return Act::Util::redirect( $Request{args}{return_url} )
+                if $Request{args}{return_url};
         }
         else {
             # map errors
@@ -115,6 +124,7 @@ sub handler {
 
     # display the event submission form
     $template->variables(
+        return_url => $Request{args}{return_url},
         dates => \@dates, defined $event ? ( %$event ) : ( %$fields ),
         rooms => { %{ $Config->rooms },
                    map { $_ => localize("room_$_") } qw(venue out),
