afe647d71392293e271ab697d44e69decf474df5
Éric Cholet <eric@logilune.com>
2008-02-05 18:03:48 +0000

    Merged revisions 1756,1766-1772,1775 via svnmerge from svn://svn.mongueurs.net/act/trunk
    ........
      r1756 | book | 2008-02-03 23:09:37 +0100 (Dim, 03 fév 2008) | 4 lines
    
      A bundle for all modules needed by Act
    
      (this should be a separate distribution, when Act is on CPAN)
    ........
      r1775 | eric | 2008-02-05 18:59:07 +0100 (Mar, 05 fév 2008) | 1 line
    
      bundle += Flick::API
    ........
    
    
    git-svn-id: svn://svn.mongueurs.net/act/branches/stable@1777 67b57a05-4208-db11-a765-00306e02d86a

---
+++ lib/Bundle/Act.pm
@@ -0,0 +1,76 @@
+package Bundle::Act;
+
+$VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Act - A bundle for the Act conference management system
+
+=head1 SYNOPSIS
+
+    perl -MCPAN -e 'install Bundle::Act'
+
+=head1 CONTENT
+
+Apache
+
+Apache::AuthCookie 3.05
+
+Apache::Cookie
+
+AppConfig
+
+DBD::Pg 1.22
+
+DBI
+
+DateTime
+
+DateTime::Format::ICal
+
+DateTime::Format::Pg
+
+DateTime::TimeZone
+
+Digest::MD5
+
+Email::Valid
+
+Flickr::API
+
+HTML::Entities
+
+Imager
+
+Locale::Maketext
+
+Locale::Maketext::Lexicon
+
+Template 2.16
+
+Template::Multilingual::Parser
+
+Test::MockObject
+
+Text::Diff
+
+Text::WikiFormat
+
+Text::xSV
+
+URI 1.31
+
+Wiki::Toolkit
+
+XML::Atom 0.20
+
+=head1 DESCRIPTION
+
+This bundle lists all the non-core modules needed by Act.
+
+=cut
+
