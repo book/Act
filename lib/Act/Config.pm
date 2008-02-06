314b7002af10bb3879034ee7031d9e3c8a178551
Éric Cholet <eric@logilune.com>
2008-02-06 02:49:42 +0000

    revert, committed by mistake
    git-svn-id: svn://svn.mongueurs.net/act/trunk@1781 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Config.pm
+++ lib/Act/Config.pm
@@ -252,22 +252,9 @@ sub get_config
         }
         # max attendees reached
         if (!$closed && $ConfConfigs{$conf}->registration_max_attendees) {
-            my $sth;
-            if ($Config->payment_type eq 'NONE') {
-                $sth = $Request{dbh}->prepare_cached('SELECT COUNT(*) FROM participations WHERE conf_id=?');
-                $sth->execute( $conf );
-            }
-            else {
-                $sth = $Request{dbh}->prepare_cached(<<EOF);
-SELECT count(u.*) FROM users u WHERE (
-     EXISTS(SELECT 1 FROM participations p WHERE p.user_id=u.user_id AND p.conf_id=?)
-  OR EXISTS(SELECT 1 FROM talks t WHERE t.user_id=u.user_id AND t.conf_id=? AND t.accepted IS TRUE)
-  OR EXISTS(SELECT 1 FROM orders o WHERE o.user_id=u.user_id AND o.conf_id=? AND o.status=?)
-  OR EXISTS(SELECT 1 FROM rights r WHERE r.user_id=u.user_id AND r.conf_id=? AND r.right_id IN (?,?))
-)
-EOF
-                $sth->execute( $conf, $conf, $conf, 'paid', $conf, 'orga', 'staff' );
-            }
+            my $sth = $Request{dbh}->prepare_cached(
+                'SELECT COUNT(*) FROM participations WHERE conf_id=?' );
+            $sth->execute( $conf );
             my ($count) = $sth->fetchrow_array();
             $sth->finish();
 
