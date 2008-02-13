bfadd296f65249d831f8e9d28acd6014f6d447cb
Éric Cholet <eric@logilune.com>
2008-02-13 13:38:06 +0000

    Merged revisions 1793,1795-1796 via svnmerge from svn://svn.mongueurs.net/act/trunk
    ........
      r1793 | eric | 2008-02-10 19:17:09 +0100 (Dim, 10 fév 2008) | 1 line
    
      treasurer feature: unregister user
    ........
      r1796 | eric | 2008-02-13 14:02:17 +0100 (Mer, 13 fév 2008) | 1 line
    
      italian translation update
    ........
    
    
    git-svn-id: svn://svn.mongueurs.net/act/branches/stable@1797 67b57a05-4208-db11-a765-00306e02d86a

---
+++ lib/Act/Handler/Payment/Unregister.pm
@@ -0,0 +1,57 @@
+package Act::Handler::Payment::Unregister;
+
+use Act::Template::HTML;
+use Act::User;
+use Act::Config;
+use Act::Util;
+
+sub handler
+{
+    # for treasurers only
+    unless ($Request{user} && $Request{user}->is_treasurer)
+    {
+        $Request{status} = NOT_FOUND;
+        return;
+    }
+    unless ($Request{args}{user_id} =~ /^\d+$/) {
+        $Request{status} = NOT_FOUND;
+        return;
+    }
+    my $user = Act::User->new(
+        user_id => $Request{args}{user_id},
+        conf_id => $Request{conference},
+    );
+    unless ($user) {
+        $Request{status} = NOT_FOUND;
+        return;
+    }
+    # user logged in and registered
+    if ($Request{args}{leave}) {
+        # remove the participation to this conference
+        my $sth = $Request{dbh}->prepare_cached(
+            "DELETE FROM participations WHERE user_id=? AND conf_id=?"
+        );
+        $sth->execute($Request{args}{user_id}, $Request{conference});
+        $sth->finish();
+        $Request{dbh}->commit;
+        return Act::Util::redirect(make_uri('payments'))
+    }
+    else {
+        my $template = Act::Template::HTML->new();
+        $template->variables(user => $user);
+        $template->process('payment/unregister');
+        return;
+    }
+}
+
+1;
+
+=head1 NAME
+
+Act::Handler::Payment::Unregister - unregister a user from a conference
+
+=head1 DESCRIPTION
+
+See F<DEVDOC> for a complete discussion on handlers.
+
+=cut
