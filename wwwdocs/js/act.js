a8c48f1a568dd89378b2f6827832dc2949274e83
Éric Cholet <eric@logilune.com>
2008-04-26 23:42:07 +0000

    remove submit button is javascript my_talk toggling is enabled
    git-svn-id: svn://svn.mongueurs.net/act/trunk@1887 67b57a05-4208-db11-a765-00306e02d86a

--- wwwdocs/js/act.js
+++ wwwdocs/js/act.js
@@ -1,3 +1,6 @@
+$(document).ready(function() {
+   $("#mytalks_submit").hide();
+});
 function act_toggle_mytalk(conf_id, talk_id)
 {
     $.post("/" + conf_id + "/ajax_toggle_mytalk", {talk_id: talk_id} );
