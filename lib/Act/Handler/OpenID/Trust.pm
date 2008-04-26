059ae9ef71843a1d2f0597aa1a54bbf0c3dd7593
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2008-04-26 13:40:18 +0000

    OpenID handlers
    git-svn-id: svn://svn.mongueurs.net/act/trunk@1871 67b57a05-4208-db11-a765-00306e02d86a

---
+++ lib/Act/Handler/OpenID/Trust.pm
@@ -0,0 +1,20 @@
+package Act::Handler::OpenID::Trust;
+
+use strict;
+use Act::Config;
+use Act::Util;
+use URI::Escape;
+
+sub handler {
+    ## We're logged in, so just redirect, until we have actual trusting.
+    my $uri = $Request{base_url} . make_uri('openid') . '?openid.mode=checkid_setup&openid.identity=';
+    $uri .= URI::Escape::uri_escape($Request{args}{identity});
+    $uri .= '&openid.return_to=';
+    $uri .= URI::Escape::uri_escape($Request{args}{return_to});
+    $uri .= '&openid.trust_root=';
+    $uri .= URI::Escape::uri_escape($Request{args}{trust_root});
+
+    Act::Util::redirect($uri);
+}
+
+1;
