8ea797541e4f1f8b1b23ac42e6d8c8288893c27b
Éric Cholet <eric@logilune.com>
2008-08-04 05:52:55 +0000

    use Data::ICal
    git-svn-id: svn://svn.mongueurs.net/act/trunk@1976 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/Talk/Import.pm
+++ lib/Act/Handler/Talk/Import.pm
@@ -3,8 +3,9 @@ use strict;
 
 use Apache::Constants qw(NOT_FOUND);
 use DateTime;
+use Data::ICal;
+use Data::ICal::DateTime;
 use DateTime::Format::Pg;
-use DateTime::Format::ICal;
 
 use Act::Config;
 use Act::Event;
@@ -23,54 +24,40 @@ sub handler
         && $Request{r}->upload()
         && defined(my $fh = $Request{r}->upload()->fh()))
     {
+        local $/ = undef;
+        my $data = <$fh>;
+        close $fh;
+    
         # process uploaded ics data
+        my $cal = Data::ICal->new(data => $data);
         my (%timeslot, @timeslots);
-        while (my $line = <$fh>) {
-            $line =~ s/\n+$//;
-            $line =~ s/\r+$//;
-            if ($line eq 'BEGIN:VEVENT') {
-                %timeslot = ();
-            }
-            elsif ($line =~ /^DTSTART.*:([\dT]+)$/) {
-                $timeslot{dtstart} = $1;
-            }
-            elsif ($line =~ /^DTEND.*:([\dT]+)$/) {
-                $timeslot{dtend} = $1;
-            }
-            elsif ($line =~ /^SUMMARY:(\w+)-(\d+)-/) {
-                if ($1 eq 'talk') {
-                    $timeslot{id_name} = 'talk_id';
-                    $timeslot{id} = $2;
-                    $timeslot{type} = 'Act::Talk';
-                }
-                elsif ($1 eq 'event') {
-                    $timeslot{id_name} = 'event_id';
-                    $timeslot{id} = $2;
-                    $timeslot{type} = 'Act::Event';
-                }
-            }
-            elsif ($line eq 'END:VEVENT' && $timeslot{type}) {
-                # process event
-                my $e = $timeslot{type}->new($timeslot{id_name} => $timeslot{id}, conf_id => $Request{conference});
-                if ($e && ($timeslot{type} ne 'talk' || !$e->lightning)) {
-                    my $dt1 = $e->datetime;
-                    my $dt2 = DateTime::Format::ICal->parse_datetime($timeslot{dtstart});
-                    # update talk with new datetime
-                    if (!$dt1 || DateTime->compare($dt1, $dt2)) {
-                        $dt1 = DateTime::Format::Pg->format_datetime($dt1) if $dt1;
-                        $dt2 = DateTime::Format::Pg->format_datetime($dt2);
-                        $e->update(datetime => $dt2);
-                        push @timeslots, { 
-                            %$e,
-                            %timeslot,
-                            dt1   => $dt1,
-                            dt2   => $dt2,
-                        };
-                    }
+        for my $event ($cal->events) {
+            my $uid = $event->property('uid')->[0]->value;
+            next unless $uid;
+            my ($type, $id) = (split '/', $uid)[-2, -1];
+            next unless $id && ($type eq 'talk' || $type eq 'event');
+
+            $timeslot{id_name} = $type ."_id";
+            $timeslot{id} = $id;
+            $timeslot{type} = 'Act::' . ucfirst $type;
+
+            # process event
+            my $e = $timeslot{type}->new($type ."_id" => $id, conf_id => $Request{conference});
+            if ($e && ($timeslot{type} ne 'talk' || !$e->lightning)) {
+                my $dt1 = $e->datetime;
+                my $dt2 = $event->start;
+                # update talk with new datetime
+                if ($dt2 && (!$dt1 || DateTime->compare($dt1, $dt2))) {
+                    $e->update(datetime => DateTime::Format::Pg->format_datetime($dt2));
+                    push @timeslots, { 
+                        %$e,
+                        %timeslot,
+                        dt1   => $dt1,
+                        dt2   => $dt2,
+                    };
                 }
             }
         }
-        close $fh;
         $template->variables(timeslots => \@timeslots) if @timeslots;
     }
     # display results
