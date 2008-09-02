b26bb3901bdd3d272645f19026a4a7d8893f50d2
Éric Cholet <eric@logilune.com>
2008-09-02 13:28:50 +0000

    Merged revisions 2029 via svnmerge from svn://svn.mongueurs.net/act/trunk
    ........
      r2029 | eric | 2008-08-21 14:43:46 +0200 (Jeu, 21 aoû 2008) | 3 lines
    
      talk ical export refactoring
      patch by Jesse Vincent
    ........
    
    
    git-svn-id: svn://svn.mongueurs.net/act/branches/stable@2045 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/Talk/ExportIcal.pm
+++ lib/Act/Handler/Talk/ExportIcal.pm
@@ -14,73 +14,119 @@ use Act::Talk;
 use Act::TimeSlot;
 use Act::Util;
 
-sub handler
-{
+sub handler {
+
     # access control
-    unless ($Request{user} && $Request{user}->is_orga || $Config->talks_show_schedule) {
+    unless ( $Request{user} && $Request{user}->is_orga
+        || $Config->talks_show_schedule )
+    {
         $Request{status} = FORBIDDEN;
         return;
     }
+    my $timeslots = _get_timeslots();
+
+    # generate iCal events
+    my $entry_defaults = _get_cal_entry_defaults();
+
+    my $cal = _setup_calendar_obj();
+    for my $ts (@$timeslots) {
+        next unless _viewable($ts);
+        my $event = _build_event( $ts => $entry_defaults );
+        $cal->add_entry($event);
+    }
+    _output($cal);
+}
+
+sub _output {
+    my $cal = shift;
+    $Request{r}->send_http_header('text/calendar; charset=UTF-8');
+    $Request{r}->print( $cal->as_string() );
+}
+
+sub _get_timeslots {
+
     # get all talks/events
     my $timeslots = [
         map {
             $_->{type} = ref;
-            $_->{id}   = $_->{talk_id} || $_->{event_id};
+            $_->{id} = $_->{talk_id} || $_->{event_id};
             bless $_, 'Act::TimeSlot';
-        }
-        @{ Act::Event->get_events(conf_id => $Request{conference}) },
+            } @{ Act::Event->get_events( conf_id => $Request{conference} ) },
         grep !$_->{lightning},
-        @{ Act::Talk->get_talks(conf_id => $Request{conference}) }
+        @{ Act::Talk->get_talks( conf_id => $Request{conference} ) }
     ];
-    # generate iCal events
+
+    return $timeslots;
+}
+
+sub _get_cal_entry_defaults {
     my %defaults = (
-        datetime => DateTime::Format::Pg->parse_timestamp($Config->talks_start_date),
-        duration => (sort { $a <=> $b } keys %{$Config->talks_durations})[0],
+        datetime =>
+            DateTime::Format::Pg->parse_timestamp( $Config->talks_start_date ),
+        duration =>
+            ( sort { $a <=> $b } keys %{ $Config->talks_durations } )[0],
     );
+    return \%defaults;
+}
+
+sub _setup_calendar_obj {
+
     my $cal = Data::ICal->new();
     $cal->add_properties(
-        calscale        => 'GREGORIAN',
-        'X-WR-CALNAME'  => $Config->name->{$Request{language}},
+        calscale       => 'GREGORIAN',
+        'X-WR-CALNAME' => $Config->name->{ $Request{language} },
     );
-    for my $ts (@$timeslots) {
-        next unless ($Request{user} && $Request{user}->is_orga)
-                 || (   ($ts->type ne 'Act::Talk' || $ts->{accepted})
-                     && $ts->datetime && $ts->duration && $ts->room
-                    );
-        # set defaults
-        $ts->{$_} ||= $defaults{$_} for keys %defaults;
-        # compute end time
-        my $dtstart = $ts->datetime;
-        my $dtend = $dtstart->clone;
-        $dtend->add(minutes => $ts->duration);
-        # uid is used to identify this event
-        # (see Act::Handler::Talk::Import)
-        (my $type = $ts->type) =~ s/^Act:://;
-        my $url = $Config->general_full_uri . join('/', lc($type), $ts->{id});
-        my $event = Data::ICal::Entry::Event->new();
-        $event->start($dtstart);
-        $event->end($dtend);
-        $event->add_properties(
-            summary => $ts->title,
-            uid     => $url,
-            url     => $url,
-            description =>
-                join '',
-                map {  $_->{text} ? $_->{text}
-                     : $_->{talk} ? $_->{talk}->title
-                     : $_->{user} ? $_->{user}->pseudonymous && $_->{user}->nick_name
-                                        || join(' ', $_->{user}->first_name, $_->{user}->last_name)
-                     : undef
-                }
-                @{ Act::Abstract::chunked( $ts->abstract ) }
-        );
-        $event->add_properties(
-            location => $Config->rooms->{$ts->room},
-        ) if $ts->room;
-        $cal->add_entry($event);
-    }
-    $Request{r}->send_http_header('text/calendar; charset=UTF-8');
-    $Request{r}->print( $cal->as_string() );
+
+    return $cal;
+}
+
+sub _viewable {
+    my $ts = shift;
+    return 0
+        unless ( $Request{user} && $Request{user}->is_orga )
+        || ( ( $ts->type ne 'Act::Talk' || $ts->{accepted} )
+        && $ts->datetime
+        && $ts->duration
+        && $ts->room );
+    return 1;
+
+}
+
+sub _build_event {
+    my $ts             = shift;
+    my $entry_defaults = shift;
+
+    # set defaults
+    $ts->{$_} ||= $entry_defaults->{$_} for keys %$entry_defaults;
+
+    # compute end time
+    my $dtstart = $ts->datetime;
+    my $dtend   = $dtstart->clone;
+    $dtend->add( minutes => $ts->duration );
+
+    # uid is used to identify this event
+    # (see Act::Handler::Talk::Import)
+    ( my $type = $ts->type ) =~ s/^Act:://;
+    my $url = $Config->general_full_uri . join( '/', lc($type), $ts->{id} );
+    my $event = Data::ICal::Entry::Event->new();
+    $event->start($dtstart);
+    $event->end($dtend);
+    $event->add_properties(
+        summary     => $ts->title,
+        uid         => $url,
+        url         => $url,
+        description => join '',
+        map {
+                  $_->{text} ? $_->{text}
+                : $_->{talk} ? $_->{talk}->title
+                : $_->{user} ? $_->{user}->pseudonymous && $_->{user}->nick_name
+                || join( ' ', $_->{user}->first_name, $_->{user}->last_name )
+                : undef
+            } @{ Act::Abstract::chunked( $ts->abstract ) }
+    );
+    $event->add_properties( location => $Config->rooms->{ $ts->room }, )
+        if $ts->room;
+    return $event;
 }
 
 1;
