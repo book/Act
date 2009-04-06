6f29e7ff116bf7a2287ebe241bed0488c9c1c68b
Éric Cholet <eric@logilune.com>
2009-04-06 15:04:36 +0000

    javascript micro templates
    git-svn-id: svn://svn.mongueurs.net/act/trunk@2115 67b57a05-4208-db11-a765-00306e02d86a

--- templates/js/mytalks.js
+++ templates/js/mytalks.js
@@ -1,42 +1,24 @@
 <script type="text/javascript">
 if (window.act) {
     toggle_image = function(elem, talk_id, set) {
-        var img_name = set ? 'picked' : 'unpicked';
         [% titles = [ loc('remove from personal schedule'), loc('add to personal schedule') ] %]
-        var title = set ? '[% titles.0.replace("'", "\\'") %]' : '[% titles.1.replace("'", "\\'") %]';
-        $(elem).replaceWith(
-            $(
-                '<img class="mtbutton" src="/images/' + img_name + '.gif"'
-              + ' title="' + title + '"'
-              + ' onClick ="toggle_talk(this,' + talk_id + ',' + set + ');" />'
-             )
-        );
-        $("#my-"+talk_id+"-text").replaceWith(
-            $(
-              '<span id="my-'+talk_id+'-text">'+title+'</span>'
-             )
-        );
+        var data = { img_name: set ? 'picked' : 'unpicked',
+                     title:    set ? '[% titles.0.replace("'", "\\'") %]' : '[% titles.1.replace("'", "\\'") %]',
+                     talk_id:  talk_id,
+                     set:      set
+        };
+        $(elem).replaceWith( act.template("tpl_mtbutton", data) );
+        $("#my-"+talk_id+"-text").replaceWith( act.template("tpl_imgtitle", data) );
     };
     toggle_count = function(talk_id, set) {
         var elemcount = "#starcount-" + talk_id;
-        var oldcount = parseInt($(elemcount + " > font").text()) || 0;
-        var newcount = oldcount + (set ? 1 : -1);
-        if (newcount) {
-            $(elemcount).replaceWith(
-                $(
-                   '<span id="starcount-'+talk_id+'" style="white-space:nowrap"><font size="-1">'+newcount+
-                   '</font><img style="vertical-align:middle" src="/images/picked.gif" /></span>'
-                 )
-            );
-        }
-        else {
-            $(elemcount).replaceWith(
-                $(
-                   '<span id="starcount-"'+talk_id+'"></span>'
-                 )
-            );
-        }
-    }
+        $(elemcount).replaceWith(
+            act.template("tpl_starcount",
+                         { talk_id: talk_id,
+                           count: (parseInt($(elemcount + " > font").text()) || 0) + (set ? 1 : -1)
+                         }
+                        ));
+    };
     toggle_talk = function(elem, talk_id, set) {
         $.post(act.make_uri('[% global.request.conference %]', 'updatemytalks_a'), {talk_id: talk_id} );
         toggle_image(elem, talk_id, !set);
@@ -49,4 +31,18 @@ if (window.act) {
         });
     });
 }
+</script>
+
+[%# javascript templates %]
+<script type="text/html" id="tpl_mtbutton">
+<img class="mtbutton" src="/images/<%=img_name%>.gif" title="<%=title%>"
+     onClick ="toggle_talk(this,<%=talk_id%>,<%=set%>);" />
+</script>
+
+<script type="text/html" id="tpl_imgtitle">
+<span id="my-<%=talk_id%>-text"><%=title%></span>
+</script>
+
+<script type="text/html" id="tpl_starcount">
+<span id="starcount-<%=talk_id%>" style="white-space:nowrap"><% if (count) { %><font size="-1"><%=count%></font><img style="vertical-align:middle" src="/images/picked.gif" /><% } %></span>
 </script>
