68f5b5f6887745badb05ff4b816e51c96bd97e14
Éric Cholet <eric@logilune.com>
2009-04-11 12:09:45 +0000

    Merged revisions 2115-2116 via svnmerge from svn://svn.mongueurs.net/act/trunk
    ........
      r2115 | eric | 2009-04-06 17:04:36 +0200 (Lun, 06 avr 2009) | 1 line
    
      javascript micro templates
    ........
      r2116 | eric | 2009-04-06 18:59:13 +0200 (Lun, 06 avr 2009) | 1 line
    
      javascript tweaks
    ........
    
    
    git-svn-id: svn://svn.mongueurs.net/act/branches/stable@2120 67b57a05-4208-db11-a765-00306e02d86a

--- templates/js/mytalks.js
+++ templates/js/mytalks.js
@@ -1,42 +1,28 @@
 <script type="text/javascript">
 if (window.act) {
+    var titles = [
+        [% FOREACH x IN ['remove from personal schedule', 'add to personal schedule'] %]
+            '[% loc(x).replace("'", "\\'") %]',
+        [% END %]
+    ];
     toggle_image = function(elem, talk_id, set) {
-        var img_name = set ? 'picked' : 'unpicked';
-        [% titles = [ loc('remove from personal schedule'), loc('add to personal schedule') ] %]
-        var title = set ? '[% titles.0.replace("'", "\\'") %]' : '[% titles.1.replace("'", "\\'") %]';
-        $(elem).replaceWith(
-            $(
-                '<img class="mtbutton" src="/images/' + img_name + '.gif"'
-              + ' title="' + title + '"'
-              + ' onClick ="toggle_talk(this,' + talk_id + ',' + set + ');" />'
-             )
-        );
-        $("#my-"+talk_id+"-text").replaceWith(
-            $(
-              '<span id="my-'+talk_id+'-text">'+title+'</span>'
-             )
-        );
+        var data = { img_name: set ? 'picked' : 'unpicked',
+                     title:    titles[set ? 0 : 1 ],
+                     talk_id:  talk_id,
+                     set:      set
+        };
+        $(elem).replaceWith( act.template("tpl_mtbutton", data) ); [%# schedule page %]
+        $("#my-"+talk_id+"-text").replaceWith( act.template("tpl_mtlabel", data) ); [%# talk page %]
     };
     toggle_count = function(talk_id, set) {
         var elemcount = "#starcount-" + talk_id;
-        var oldcount = parseInt($(elemcount + " > font").text()) || 0;
-        var newcount = oldcount + (set ? 1 : -1);
-        if (newcount) {
-            $(elemcount).replaceWith(
-                $(
-                   '<span id="starcount-'+talk_id+'" style="white-space:nowrap"><font size="-1">'+newcount+
-                   '</font><img style="vertical-align:middle" src="/images/picked.gif" /></span>'
-                 )
-            );
-        }
-        else {
-            $(elemcount).replaceWith(
-                $(
-                   '<span id="starcount-"'+talk_id+'"></span>'
-                 )
-            );
-        }
-    }
+        $(elemcount).replaceWith(
+            act.template("tpl_starcount",
+                         { talk_id: talk_id,
+                           count: (parseInt($(elemcount + " > .starcount").text()) || 0) + (set ? 1 : -1)
+                         }
+                        ));
+    };
     toggle_talk = function(elem, talk_id, set) {
         $.post(act.make_uri('[% global.request.conference %]', 'updatemytalks_a'), {talk_id: talk_id} );
         toggle_image(elem, talk_id, !set);
@@ -49,4 +35,18 @@ if (window.act) {
         });
     });
 }
+</script>
+
+[%# javascript templates %]
+<script type="text/html" id="tpl_mtbutton">
+<img class="mtbutton" src="/images/<%=img_name%>.gif" title="<%=title%>"
+     onClick ="toggle_talk(this,<%=talk_id%>,<%=set%>);" />
+</script>
+
+<script type="text/html" id="tpl_mtlabel">
+<span id="my-<%=talk_id%>-text"><%=title%></span>
+</script>
+
+<script type="text/html" id="tpl_starcount">
+<span id="starcount-<%=talk_id%>" style="white-space:nowrap"><% if (count) { %><span class="starcount"><%=count%></span><img style="vertical-align:middle" src="/images/picked.gif" /><% } %></span>
 </script>
