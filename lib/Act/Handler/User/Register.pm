fffdaefc277da68c18d2159b0d0216e2a0e47b0c
Sébastien Aperghis-Tramoni <saper@cpan.org>
2010-06-04 21:52:50 +0000

    Update the regexp to validate the t-shirt size. This one isn't as narrow than the previous one, but the actual regexp is three times longer.
    git-svn-id: svn://svn.mongueurs.net/act/trunk@2221 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/User/Register.pm
+++ lib/Act/Handler/User/Register.pm
@@ -35,7 +35,7 @@ my $form = Act::Form->new(
   constraints => {
      login => sub { $_[0] =~ /^[A-Za-z0-9_]{3,}$/ },
      email => 'email',
-     tshirt => sub { $_[0] =~ /^(?:S|M|X{0,3}L)$/ },
+     tshirt => sub { $_[0] =~ /^(?:X?S|M|[456]?X{0,3}LT?)(?: \(W\))?$/ },
   }
 );
 
