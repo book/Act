0131c460e86129e204d25c6feca4f4bc5ca98994
Sébastien Aperghis-Tramoni <saper@cpan.org>
2011-06-04 17:24:24 +0000

    merged changes 2247,2248,2249,2250,2252,2253,2254,2255,2256,2257: - add functions headers and empty lines to make the code a bit more readable. - add the number and list of known attendees to this talk. - add the speaker's name. - simplify the code by using public_name().
    git-svn-id: svn://svn.mongueurs.net/act/branches/stable@2262 67b57a05-4208-db11-a765-00306e02d86a

--- lib/Act/Handler/Talk/ExportIcal.pm
+++ lib/Act/Handler/Talk/ExportIcal.pm
@@ -14,8 +14,11 @@ use Act::Talk;
 use Act::TimeSlot;
 use Act::Util;
 
-sub handler {
 
+#
+# handler()
+# -------
+sub handler {
     # access control
     unless ( $Request{user} && $Request{user}->is_talks_admin
         || $Config->talks_show_schedule )
@@ -23,47 +26,74 @@ sub handler {
         $Request{status} = FORBIDDEN;
         return;
     }
+
     my $timeslots = _get_timeslots();
     export($timeslots);
 }
 
+
+#
+# export()
+# ------
 sub export {
     my $timeslots = shift;
 
     # generate iCal events
     my $entry_defaults = _get_cal_entry_defaults();
-
     my $cal = _setup_calendar_obj();
+
     for my $ts (@$timeslots) {
         next unless _viewable($ts);
         my $event = _build_event( $ts => $entry_defaults );
         $cal->add_entry($event);
     }
+
     _output($cal);
 }
 
+
+#
+# _output()
+# -------
 sub _output {
     my $cal = shift;
     $Request{r}->send_http_header('text/calendar; charset=UTF-8');
     $Request{r}->print( $cal->as_string() );
 }
 
+
+#
+# _get_timeslots()
+# --------------
 sub _get_timeslots {
+    my @events = @{ Act::Event->get_events( conf_id => $Request{conference} ) };
+    my @talks  = grep !$_->{lightning},
+        @{ Act::Talk->get_talks( conf_id => $Request{conference} ) };
 
     # get all talks/events
     my $timeslots = [
         map {
             $_->{type} = ref;
             $_->{id} = $_->{talk_id} || $_->{event_id};
+
+            if ($_->{type} eq "Act::Talk") {
+                $_->{user}  = Act::User->new( user_id => $_->user_id );
+                $_->{track} = Act::Track->new( track_id => $_->track_id )
+                    if $_->{track_id};
+                $_->{stars} = $_->stars;
+            }
+
             bless $_, 'Act::TimeSlot';
-            } @{ Act::Event->get_events( conf_id => $Request{conference} ) },
-        grep !$_->{lightning},
-        @{ Act::Talk->get_talks( conf_id => $Request{conference} ) }
+        } @events, @talks
     ];
 
     return $timeslots;
 }
 
+
+#
+# _get_cal_entry_defaults()
+# -----------------------
 sub _get_cal_entry_defaults {
     my %defaults = (
         datetime =>
@@ -71,12 +101,17 @@ sub _get_cal_entry_defaults {
         duration =>
             ( sort { $a <=> $b } keys %{ $Config->talks_durations } )[0],
     );
+
     return \%defaults;
 }
 
-sub _setup_calendar_obj {
 
+#
+# _setup_calendar_obj()
+# -------------------
+sub _setup_calendar_obj {
     my $cal = Data::ICal->new();
+
     $cal->add_properties(
         calscale       => 'GREGORIAN',
         'X-WR-CALNAME' => $Config->name->{ $Request{language} },
@@ -85,18 +120,27 @@ sub _setup_calendar_obj {
     return $cal;
 }
 
+
+#
+# _viewable()
+# ---------
 sub _viewable {
     my $ts = shift;
+
     return 0
         unless ( $Request{user} && $Request{user}->is_talks_admin )
         || ( ( $ts->type ne 'Act::Talk' || $ts->{accepted} )
         && $ts->datetime
         && $ts->duration
         && $ts->room );
-    return 1;
 
+    return 1;
 }
 
+
+#
+# _build_event()
+# ------------
 sub _build_event {
     my $ts             = shift;
     my $entry_defaults = shift;
@@ -116,23 +160,40 @@ sub _build_event {
     my $event = Data::ICal::Entry::Event->new();
     $event->start($dtstart);
     $event->end($dtend);
+
     $event->add_properties(
         summary     => $ts->title,
         uid         => $url,
         url         => $url,
         description => join '',
-        map {
+            map {
                   $_->{text} ? $_->{text}
                 : $_->{talk} ? $_->{talk}->title
-                : $_->{user} ? $_->{user}->pseudonymous && $_->{user}->nick_name
-                || join( ' ', $_->{user}->first_name, $_->{user}->last_name )
+                : $_->{user} ? $_->{user}->public_name
                 : undef
             } @{ Act::Abstract::chunked( $ts->abstract ) }
     );
-    $event->add_properties( location => $Config->rooms->{ $ts->room }, )
+
+    $event->add_properties( location => $Config->rooms->{ $ts->room } )
         if $ts->room;
+
+    # for a talk, add a few more properties
+    if ($ts->{type} eq "Act::Talk") {
+        # add the speaker's name
+        $event->add_properties(organizer => $ts->{user}->public_name);
+
+        # add the list of known attendees
+        my @attendees = @{ Act::User->attendees($ts->{id}) };
+        $event->add_properties(comment => @attendees." attendees");
+
+        for my $user (@attendees) {
+            $event->add_properties(attendee => $user->public_name);
+        }
+    }
+
     return $event;
 }
+
 
 1;
 __END__
