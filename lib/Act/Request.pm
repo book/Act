b8aec39da5f0872af000516d33a35f75d0d3eb4f
Rob Hoelz <rob@hoelz.ro>
2011-07-26 11:40:13 -0700

    Create an application-specific subclass of Plack::Request

---
+++ lib/Act/Request.pm
@@ -0,0 +1,17 @@
+package Act::Request;
+
+use strict;
+use warnings;
+use parent 'Plack::Request';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Act::Request - A subclass of Plack::Request that handles like Apache::Request
+
+=head1 DESCRIPTION
+
+=cut
