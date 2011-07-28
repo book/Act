fa19141925ea450f1c8b746b53dd6c492d5b4512
Graham Knop <haarg@haarg.org>
2011-07-28 08:16:03 -0500

    first concept for PSGI dispatch code

---
+++ lib/Act/Middleware/Language.pm
@@ -0,0 +1,78 @@
+package Act::Middleware::Language;
+use strict;
+use warnings;
+
+use parent qw(Plack::Middleware);
+use Plack::Request;
+
+sub call {
+    my $self = shift;
+    my $env = shift;
+    my $req = Plack::Request->new($env);
+
+    my $config = $env->{'act.config'};
+    my $langs = $config->languages;
+
+    my $language;
+
+    # check session
+    my $s = $env->{'psgix.session'}{'act'};
+    if ($s && $s->{language} && $langs->{$s->{language}}) {
+        $language = $s->{language};
+    }
+
+    # override in query string
+    my $redirect;
+    my $force_language = $req->param('language');
+    if ($force_language && $langs->{$force_language} ) {
+        $redirect = 1;
+        $language = $s->{language} = $force_language;
+    }
+
+    # otherwise try one of the browser's languages
+    unless ($language) {
+        eval {
+            $req->headers->scan(sub {
+                my ($k, $v) = @_;
+                return
+                    unless $k eq 'Accept-Language';
+                $v =~ s/;.*$//;
+                $v =~ s/-.*$//;
+                if ($v && $langs->{$v}) {
+                    $language = $c->{language} = $v;
+                    die [];
+                }
+            });
+        };
+    }
+
+    # last resort, use our default language
+    $language ||= $config->general_default_language;
+
+    # use optional variant
+    $language = $config->language_variants->{$language} || $language;
+
+    # remember it for this request
+    $env->{'act.language'} = $language;
+
+    # fetch localization handle
+    $env->{'act.loc'} = Act::I18N->get_handle($language);
+
+    if ($redirect) {
+        my $uri = $req->uri;
+        my @query = $uri->query_form;
+        for (my $i; $i < @query; $i+=2 ) {
+            if ($query[$i] eq 'language') {
+                splice @query, $i, 2;
+            }
+        }
+        $uri->query_form(\@query);
+        my $resp = Plack::Response->new;
+        $resp->redirect($uri->as_string);
+        return $resp->finalize;
+    }
+
+    $self->app->($env);
+}
+
+
