afef5084f0deff26186e4f387c214ae8dc99e513
Rob Hoelz <rob@hoelz.ro>
2011-08-20 13:07:21 -0500

    Convert User::Stats handler

--- lib/Act/Handler/User/Stats.pm
+++ lib/Act/Handler/User/Stats.pm
@@ -1,4 +1,8 @@
 package Act::Handler::User::Stats;
+
+use strict;
+use 'Act::Handler';
+
 use Act::Config;
 use Act::Template::HTML;
 use Act::User;
@@ -66,6 +70,7 @@ sub handler {
     my $template = Act::Template::HTML->new();
     $template->variables( stats => $stats );
     $template->process('user/stats');
+    return;
 }
 
 1;
