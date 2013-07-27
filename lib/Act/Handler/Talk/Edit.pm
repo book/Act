book/Act

commit b8f2db21258d1a449e120aa9837cf4dc3bef7bf3

talk modification: check talk ownership earlier
git-svn-id: svn://svn.mongueurs.net/act/trunk@1452 67b57a05-4208-db11-a765-00306e02d86a

lib/Act/Handler/Talk/Edit.pm
@@ -67,7 +67,8 @@ sub handler {
     # and edit existing talks when edition_open or submission_open
     #
     unless ($Request{user}->is_orga) {
-        unless ( ($talk && ($Config->talks_edition_open || $Config->talks_submissions_open))
+        unless ( ($talk && $talk->user_id == $Request{user}->user_id
+                        && ($Config->talks_edition_open || $Config->talks_submissions_open))
                 || $Config->talks_submissions_open )
         {
             $Request{status} = NOT_FOUND;
@@ -116,11 +117,6 @@ sub handler {
         }
         # normal user
         else {
-            # can only edit his own talks
-            if( defined $talk and $talk->user_id != $Request{user}->user_id ) {
-                $Request{status} = FORBIDDEN;
-                return;
-            }
             # cannot comment a talk or change the date/room
             delete @{$fields}{qw( is_lightning date time room )};
             # cannot modify the duration

commit e9c5257594f7eb69c4f935fb14fadb1bc79b46d7

prevent a user to edit someone else's talk
the bug was introduced by b8f2db21258d1a449e120aa9837cf4dc3bef7bf3
and caused by the unnecessary complexity of the test,
which has nested unless with complicated conditions

this patch only fixes the bug, not the unnecessary complexity of the test

lib/Act/Handler/Talk/Edit.pm
@@ -79,7 +79,7 @@ sub handler {
     unless ($Request{user}->is_talks_admin) {
         unless ( ($talk && $talk->user_id == $Request{user}->user_id
                         && ($Config->talks_edition_open || $Config->talks_submissions_open))
-                || $Config->talks_submissions_open )
+                || (!$talk && $Config->talks_submissions_open ))
         {
             $Request{status} = NOT_FOUND;
             return;