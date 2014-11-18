0e7ebb703f8f98c80ad041b42a18ada06717e945
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2014-11-18 22:13:37 +0200

    sugar coated Act::Schema::Result::NewsItem

--- lib/Act/Schema/Result/NewsItem.pm
+++ lib/Act/Schema/Result/NewsItem.pm
@@ -1,75 +1,80 @@
-use utf8;
 package Act::Schema::Result::NewsItem;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
+use utf8;
+use 'Act::Schema::Candy';
 
 =head1 NAME
 
 Act::Schema::Result::NewsItem
 
-=cut
+=head1 DESCRIPTION
 
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
+A 'per language' translation for the L<Act::Schema::Result::News> entry.
 
 =head1 TABLE: C<news_items>
 
 =cut
 
-__PACKAGE__->table("news_items");
+table "news_items";
 
 =head1 ACCESSORS
 
 =head2 news_item_id
 
-  data_type: 'integer'
-  is_auto_increment: 1
-  is_nullable: 0
-  sequence: 'news_items_news_item_id_seq'
-
-=head2 news_id
-
-  data_type: 'integer'
-  is_foreign_key: 1
-  is_nullable: 0
-
-=head2 lang
-
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 title
-
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 text
-
-  data_type: 'text'
-  is_nullable: 0
+Primary Key for language specific translations for News publications
 
 =cut
 
-__PACKAGE__->add_columns(
-  "news_item_id",
-  {
-    data_type         => "integer",
-    is_auto_increment => 1,
-    is_nullable       => 0,
-    sequence          => "news_items_news_item_id_seq",
-  },
-  "news_id",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
-  "lang",
-  { data_type => "text", is_nullable => 0 },
-  "title",
-  { data_type => "text", is_nullable => 0 },
-  "text",
-  { data_type => "text", is_nullable => 0 },
-);
+column "news_item_id" => {
+    data_type          => 'integer',
+    is_auto_increment  => 1,
+    is_nullable        => 0,
+    sequence           => 'news_items_news_item_id_seq',
+};
+
+=head2 news_id
+
+L<Act::Schema::Result::News> ID
+
+=cut
+
+column "news_id" => {
+    data_type          => 'integer',
+    is_foreign_key     => 1,
+    is_nullable        => 0,
+};
+
+=head2 lang
+
+ISO language identifier
+
+=cut
+
+column "lang" => {
+    data_type          => 'text',
+    is_nullable        => 0,
+};
+
+=head2 title
+
+Language specific title for the News publication
+
+=cut
+
+column "title" => {
+      data_type        => 'text',
+      is_nullable      => 0,
+};
+
+=head2 text
+
+Language specific text for the News publication
+
+=cut
+
+column "text" => {
+    data_type          => 'text',
+    is_nullable        => 0,
+};
 
 =head1 PRIMARY KEY
 
@@ -81,7 +86,7 @@ __PACKAGE__->add_columns(
 
 =cut
 
-__PACKAGE__->set_primary_key("news_item_id");
+primary_key "news_item_id";
 
 =head1 UNIQUE CONSTRAINTS
 
@@ -97,29 +102,25 @@ __PACKAGE__->set_primary_key("news_item_id");
 
 =cut
 
-__PACKAGE__->add_unique_constraint("news_items_news_id_key", ["news_id", "lang"]);
+unique_constraint "news_items_news_id_key" => ["news_id", "lang"];
 
 =head1 RELATIONS
 
 =head2 news
 
-Type: belongs_to
-
-Related object: L<Act::Schema::Result::News>
+belongs_to L<Act::Schema::Result::News>
 
 =cut
 
-__PACKAGE__->belongs_to(
-  "news",
-  "Act::Schema::Result::News",
+belongs_to "news" => "Act::Schema::Result::News",
   { news_id => "news_id" },
   { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" },
 );
 
+=head1 COPYRIGHT
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-11-18 10:52:44
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:wpQ7gOuilW4YYg0JqSEq+w
+(c) 2014 - Th.J. van Hoesel - THEMA-MEDIA NL
 
+=cut
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
