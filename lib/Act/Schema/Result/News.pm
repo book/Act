03be5859e46c157530d5ed7cb93708e7e3f69467
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2014-11-18 21:50:14 +0200

    sugar coated Act::Schema::Result::News

--- lib/Act/Schema/Result/News.pm
+++ lib/Act/Schema/Result/News.pm
@@ -1,75 +1,80 @@
-use utf8;
 package Act::Schema::Result::News;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
+use utf8;
+use 'Act::Schema::Candy';
 
 =head1 NAME
 
 Act::Schema::Result::News
 
-=cut
+=head1 DESCRIPTION
 
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
+News Entries index, which can have per language translated L<Act::Schema::Result::NewsItem>s
 
 =head1 TABLE: C<news>
 
 =cut
 
-__PACKAGE__->table("news");
+table "news";
 
 =head1 ACCESSORS
 
 =head2 news_id
 
-  data_type: 'integer'
-  is_auto_increment: 1
-  is_nullable: 0
-  sequence: 'news_news_id_seq'
-
-=head2 conf_id
-
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 datetime
-
-  data_type: 'timestamp'
-  is_nullable: 0
-
-=head2 user_id
-
-  data_type: 'integer'
-  is_nullable: 0
-
-=head2 published
-
-  data_type: 'boolean'
-  default_value: false
-  is_nullable: 0
+Primary key, News ID
 
 =cut
 
-__PACKAGE__->add_columns(
-  "news_id",
-  {
-    data_type         => "integer",
-    is_auto_increment => 1,
-    is_nullable       => 0,
-    sequence          => "news_news_id_seq",
-  },
-  "conf_id",
-  { data_type => "text", is_nullable => 0 },
-  "datetime",
-  { data_type => "timestamp", is_nullable => 0 },
-  "user_id",
-  { data_type => "integer", is_nullable => 0 },
-  "published",
-  { data_type => "boolean", default_value => \"false", is_nullable => 0 },
-);
+column "news_id" => {
+    data_type          => 'integer',
+    is_auto_increment  => 1,
+    is_nullable        => 0,
+    sequence           => 'news_news_id_seq',
+};
+
+=head2 conf_id
+
+Cummunity Event ID
+
+=cut
+
+column "conf_id" => {
+    data_type          => 'text',
+    is_nullable        => 0,
+};
+
+=head2 datetime
+
+Date and Time of this News publication.
+
+=cut
+
+colunm "datetime" => {
+    data_typei         => 'timestamp',
+    is_nullable        => 0,
+};
+
+=head2 user_id
+
+User ID of the author of this News publication
+
+=cut
+
+column "user_id" => {
+    data_type          => 'integer',
+    is_nullable        => 0,
+};
+
+=head2 published
+
+Flag to indicate wether or not a News article is already published or not.
+
+=cut
+
+column "published" => {
+    data_type          => 'boolean',
+    default_value      => \"false",
+    is_nullable        => 0,
+};
 
 =head1 PRIMARY KEY
 
@@ -81,29 +86,24 @@ __PACKAGE__->add_columns(
 
 =cut
 
-__PACKAGE__->set_primary_key("news_id");
+primary_key "news_id";
 
 =head1 RELATIONS
 
 =head2 news_items
 
-Type: has_many
-
-Related object: L<Act::Schema::Result::NewsItem>
+has_many L<Act::Schema::Result::NewsItem>s.
 
 =cut
 
-__PACKAGE__->has_many(
-  "news_items",
-  "Act::Schema::Result::NewsItem",
-  { "foreign.news_id" => "self.news_id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
+has_many "news_items" => "Act::Schema::Result::NewsItem",
+    { "foreign.news_id" => "self.news_id" },
+    { cascade_copy => 0, cascade_delete => 0 };
 
+=head1 COPYRIGHT
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-11-18 10:52:44
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:MbfQQ05M8GRr1ZRKlWEFkA
+(c) 2014 - Th.J. van Hoesel - THEMA-MEDIA NL
 
+=cut
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
