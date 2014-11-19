1455f336a86654d59af572a9b13e45a2114b28e8
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2014-11-19 11:22:12 +0100

    sugar coated Act::Schema::Result::PmGroup

--- lib/Act/Schema/Result/PmGroup.pm
+++ lib/Act/Schema/Result/PmGroup.pm
@@ -1,88 +1,100 @@
-use utf8;
 package Act::Schema::Result::PmGroup;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
+use utf8;
+use 'Act::Schema::Candy';
 
 =head1 NAME
 
 Act::Schema::Result::PmGroup
 
-=cut
-
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
+Representaion of a Perl Mongers group
 
 =head1 TABLE: C<pm_groups>
 
 =cut
 
-__PACKAGE__->table("pm_groups");
+table "pm_groups";
 
 =head1 ACCESSORS
 
 =head2 group_id
 
-  data_type: 'integer'
-  is_auto_increment: 1
-  is_nullable: 0
-  sequence: 'pm_groups_group_id_seq'
-
-=head2 xml_group_id
-
-  data_type: 'integer'
-  is_nullable: 1
-
-=head2 name
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 status
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 continent
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 country
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 state
-
-  data_type: 'text'
-  is_nullable: 1
+Perl Mongers Group Primary Key
 
 =cut
 
-__PACKAGE__->add_columns(
-  "group_id",
-  {
-    data_type         => "integer",
-    is_auto_increment => 1,
-    is_nullable       => 0,
-    sequence          => "pm_groups_group_id_seq",
-  },
-  "xml_group_id",
-  { data_type => "integer", is_nullable => 1 },
-  "name",
-  { data_type => "text", is_nullable => 1 },
-  "status",
-  { data_type => "text", is_nullable => 1 },
-  "continent",
-  { data_type => "text", is_nullable => 1 },
-  "country",
-  { data_type => "text", is_nullable => 1 },
-  "state",
-  { data_type => "text", is_nullable => 1 },
-);
+column "group_id" => {
+    data_type          => 'integer',
+    is_auto_increment  => 1,
+    is_nullable        => 0,
+    sequence           => 'pm_groups_group_id_seq',
+};
+
+=head2 xml_group_id
+
+XML ID as being used in the Perl Mongers Group data-file
+
+=cut
+
+column "xml_group_id" => {
+    data_type          => 'integer',
+    is_nullable        => 1,
+};
+
+=head2 name
+
+Name of the PM Group, usually the name of the city
+
+=cut
+
+column => "name" {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 status
+
+The current status of the group, unfortunatly, some Perl Monger groups become
+inactive after some while.
+
+=cut
+
+column "status" {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 continent
+
+The conitnent where the group is located.
+
+=cut
+
+column "continent" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 country
+
+The country where the group is located.
+
+=cut
+
+column "country" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 state
+
+The state where the group is located.
+
+=cut
+
+column "state" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
 
 =head1 PRIMARY KEY
 
@@ -94,12 +106,12 @@ __PACKAGE__->add_columns(
 
 =cut
 
-__PACKAGE__->set_primary_key("group_id");
+primary_key "group_id";
 
+=head1 COPYRIGHT
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-11-18 10:52:44
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:xyZA/nwtEchoZnftpx/klg
+(c) 2014 - Th.J. van Hoesel - THEMA-MEDIA NL
 
+=cut
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
