a048704ba32650e503666fd30fa8660fb011deef
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2014-11-19 19:51:48 +0100

    sugar coated Act::Schema::Result::Track

--- lib/Act/Schema/Result/Track.pm
+++ lib/Act/Schema/Result/Track.pm
@@ -1,67 +1,68 @@
-use utf8;
 package Act::Schema::Result::Track;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
+use utf8;
+use 'Act::Schema::Candy';
 
 =head1 NAME
 
 Act::Schema::Result::Track
 
-=cut
+=head1 DESCRIPTION
 
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
+TODO:
 
 =head1 TABLE: C<tracks>
 
 =cut
 
-__PACKAGE__->table("tracks");
+table "tracks";
 
 =head1 ACCESSORS
 
 =head2 track_id
 
-  data_type: 'integer'
-  is_auto_increment: 1
-  is_nullable: 0
-  sequence: 'tracks_track_id_seq'
-
-=head2 conf_id
-
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 title
-
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 description
-
-  data_type: 'text'
-  is_nullable: 1
+Primary Key
 
 =cut
 
-__PACKAGE__->add_columns(
-  "track_id",
-  {
-    data_type         => "integer",
-    is_auto_increment => 1,
-    is_nullable       => 0,
-    sequence          => "tracks_track_id_seq",
-  },
-  "conf_id",
-  { data_type => "text", is_nullable => 0 },
-  "title",
-  { data_type => "text", is_nullable => 0 },
-  "description",
-  { data_type => "text", is_nullable => 1 },
-);
+column "track_id" => {
+    data_type          => 'integer',
+    is_auto_increment  => 1,
+    is_nullable        => 0,
+    sequence           => 'tracks_track_id_seq',
+};
+
+=head2 conf_id
+
+Cummunity Event ID
+
+=cut
+
+column "conf_id" => {
+    data_type          => 'text',
+    is_nullable        => 0,
+};
+
+=head2 title
+
+Track Title
+
+=cut
+
+column "title" => {
+    data_type          => 'text',
+    is_nullable        => 0,
+};
+
+=head2 description
+
+Description of this track
+
+=cut
+
+column "description" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
 
 =head1 PRIMARY KEY
 
@@ -73,29 +74,24 @@ __PACKAGE__->add_columns(
 
 =cut
 
-__PACKAGE__->set_primary_key("track_id");
+primary_key "track_id";
 
 =head1 RELATIONS
 
 =head2 talks
 
-Type: has_many
-
-Related object: L<Act::Schema::Result::Talk>
+has_many related object: L<Act::Schema::Result::Talk>
 
 =cut
 
-__PACKAGE__->has_many(
-  "talks",
-  "Act::Schema::Result::Talk",
+has_many "talks" => "Act::Schema::Result::Talk",
   { "foreign.track_id" => "self.track_id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
+  { cascade_copy => 0, cascade_delete => 0 };
 
+=head1 COPYRIGHT
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-11-18 10:52:44
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:rPHCtjKqOhSGU6ma65tffg
+(c) 2014 - Th.J. van Hoesel - THEMA-MEDIA NL
 
+=cut
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
