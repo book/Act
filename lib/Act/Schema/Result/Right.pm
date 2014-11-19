0dc39f68d4dc13be344723d6459ef5f97aae9090
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2014-11-19 13:06:39 +0100

    sugar coated Act::Schema::Result::Schema

--- lib/Act/Schema/Result/Right.pm
+++ lib/Act/Schema/Result/Right.pm
@@ -1,76 +1,72 @@
-use utf8;
 package Act::Schema::Result::Right;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
+use utf8;
+use 'Act::Schema::Candy';
 
 =head1 NAME
 
 Act::Schema::Result::Right
 
-=cut
-
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
+Defines what privileges a user has for a specific Cummunity Event
 
 =head1 TABLE: C<rights>
 
 =cut
 
-__PACKAGE__->table("rights");
+table "rights";
 
 =head1 ACCESSORS
 
 =head2 right_id
 
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 conf_id
-
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 user_id
-
-  data_type: 'integer'
-  is_foreign_key: 1
-  is_nullable: 0
+A 'Privilege' as described in the manuals, currently there are: 'admin',
+'talks_admin', 'users_admin', 'news_admin', 'wiki_admin' and 'treasurer'.
 
 =cut
 
-__PACKAGE__->add_columns(
-  "right_id",
-  { data_type => "text", is_nullable => 0 },
-  "conf_id",
-  { data_type => "text", is_nullable => 0 },
-  "user_id",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
-);
+column "right_id" => {
+    data_type          => 'text',
+    is_nullable        => 0,
+};
+
+=head2 conf_id
+
+Cumminity Event ID
+
+=cut
+
+column "conf_id" => {
+    data_type          => 'text',
+    is_nullable        => 0,
+};
+
+=head2 user_id
+
+Foreign Key for L<Act::Schema::Result::User>
+
+=cut
+
+column "user_id" => {
+    data_type          => 'integer',
+    is_foreign_key     => 1,
+    is_nullable        => 0,
+};
 
 =head1 RELATIONS
 
 =head2 user
 
-Type: belongs_to
-
-Related object: L<Act::Schema::Result::User>
+belongs_to related object: L<Act::Schema::Result::User>
 
 =cut
 
-__PACKAGE__->belongs_to(
-  "user",
-  "Act::Schema::Result::User",
+belongs_to "user" => "Act::Schema::Result::User",
   { user_id => "user_id" },
-  { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" },
-);
+  { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" };
 
+=head1 COPYRIGHT
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-11-18 10:52:44
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:c8e7i7sslaxyNP4y9A9wjw
+(c) 2014 - Th.J. van Hoesel - THEMA-MEDIA NL
 
+=cut
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
--- lib/Act/Schema/Result/Schema.pm
+++ lib/Act/Schema/Result/Schema.pm
@@ -1,44 +1,36 @@
-use utf8;
 package Act::Schema::Result::Schema;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
+use utf8;
+use 'Act::Schema::Candy';
 
 =head1 NAME
 
 Act::Schema::Result::Schema
 
-=cut
-
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
+Holds the latest schema version as setup through L<Act::Database>.
 
 =head1 TABLE: C<schema>
 
 =cut
 
-__PACKAGE__->table("schema");
+table "schema";
 
 =head1 ACCESSORS
 
 =head2 current_version
 
-  data_type: 'integer'
-  is_nullable: 0
+Holds the actual latest version of the Act Database Schema
 
 =cut
 
-__PACKAGE__->add_columns(
-  "current_version",
-  { data_type => "integer", is_nullable => 0 },
-);
+column "current_version" => {
+    data_type          => 'integer',
+    is_nullable        => 0,
+};
 
+=head1 COPYRIGHT
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-11-18 10:52:44
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:jcSCIMqGws58QR2Su8GeCg
+(c) 2014 - Th.J. van Hoesel - THEMA-MEDIA NL
 
+=cut
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
