f921af50c8aee68187d8f1e755da6631cd59f470
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2014-11-19 14:56:06 +0100

    sugar coated Act::Schema::Result::Talk

--- lib/Act/Schema/Result/Talk.pm
+++ lib/Act/Schema/Result/Talk.pm
@@ -1,164 +1,222 @@
-use utf8;
 package Act::Schema::Result::Talk;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
+use utf8;
+use 'Act::Schema::Candy';
 
 =head1 NAME
 
 Act::Schema::Result::Talk
 
-=cut
+=head1 DESCRIPTION
 
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
+Contains all info for a specific talk. Talks are Cummunity Event related and
+althoug common practice to repeat a talk at several occasions, it needs a
+seperate entry.
 
 =head1 TABLE: C<talks>
 
 =cut
 
-__PACKAGE__->table("talks");
+table "talks";
 
 =head1 ACCESSORS
 
 =head2 talk_id
 
-  data_type: 'integer'
-  is_auto_increment: 1
-  is_nullable: 0
-  sequence: 'talks_talk_id_seq'
-
-=head2 conf_id
-
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 user_id
-
-  data_type: 'integer'
-  is_foreign_key: 1
-  is_nullable: 0
-
-=head2 title
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 abstract
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 url_abstract
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 url_talk
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 duration
-
-  data_type: 'integer'
-  is_nullable: 1
-
-=head2 lightning
-
-  data_type: 'boolean'
-  default_value: false
-  is_nullable: 0
-
-=head2 accepted
-
-  data_type: 'boolean'
-  default_value: false
-  is_nullable: 0
-
-=head2 confirmed
-
-  data_type: 'boolean'
-  default_value: false
-  is_nullable: 0
-
-=head2 comment
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 room
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 datetime
-
-  data_type: 'timestamp'
-  is_nullable: 1
-
-=head2 track_id
-
-  data_type: 'integer'
-  is_foreign_key: 1
-  is_nullable: 1
-
-=head2 level
-
-  data_type: 'integer'
-  default_value: 1
-  is_nullable: 1
-
-=head2 lang
-
-  data_type: 'text'
-  is_nullable: 1
+Primary Key
 
 =cut
 
-__PACKAGE__->add_columns(
-  "talk_id",
-  {
-    data_type         => "integer",
-    is_auto_increment => 1,
-    is_nullable       => 0,
-    sequence          => "talks_talk_id_seq",
-  },
-  "conf_id",
-  { data_type => "text", is_nullable => 0 },
-  "user_id",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
-  "title",
-  { data_type => "text", is_nullable => 1 },
-  "abstract",
-  { data_type => "text", is_nullable => 1 },
-  "url_abstract",
-  { data_type => "text", is_nullable => 1 },
-  "url_talk",
-  { data_type => "text", is_nullable => 1 },
-  "duration",
-  { data_type => "integer", is_nullable => 1 },
-  "lightning",
-  { data_type => "boolean", default_value => \"false", is_nullable => 0 },
-  "accepted",
-  { data_type => "boolean", default_value => \"false", is_nullable => 0 },
-  "confirmed",
-  { data_type => "boolean", default_value => \"false", is_nullable => 0 },
-  "comment",
-  { data_type => "text", is_nullable => 1 },
-  "room",
-  { data_type => "text", is_nullable => 1 },
-  "datetime",
-  { data_type => "timestamp", is_nullable => 1 },
-  "track_id",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
-  "level",
-  { data_type => "integer", default_value => 1, is_nullable => 1 },
-  "lang",
-  { data_type => "text", is_nullable => 1 },
-);
+column "talk_id" => {
+    data_type          => 'integer',
+    is_auto_increment  => 1,
+    is_nullable        => 0,
+    sequence           => 'talks_talk_id_seq',
+};
+
+=head2 conf_id
+
+Community Event ID
+
+=cut
+
+column "conf_id" => {
+    data_type          => 'text',
+    is_nullable        => 0,
+};
+
+=head2 user_id
+
+Foreign Key for L<Act::Schema::Result::User>.
+
+=cut
+
+column "user_id" => {
+    data_type          => 'integer',
+    is_foreign_key     => 1,
+    is_nullable        => 0,
+};
+
+=head2 title
+
+The title of this talk
+
+=cut
+
+column "title" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 abstract
+
+Description of the talk
+
+=cut
+
+column "abstract" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 url_abstract
+
+External link to the abstract
+
+=cut
+
+column "url_abstract" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 url_talk
+
+External link to more info of this talk
+
+=cut
+
+column "url_talk" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 duration
+
+Duration in . . . .
+
+=cut
+
+column "duration" => {
+    data_type          => 'integer',
+    is_nullable        => 1,
+};
+
+=head2 lightning
+
+Flag to indicate if this is a lightning talk. Lightning talks do not get their
+own entry in the schedule.
+
+=cut
+
+column "lightning" => {
+    data_type          => 'boolean',
+    default_value      => \"false",
+    is_nullable        => 0,
+};
+
+=head2 accepted
+
+Flag to indicate if this talk has been accepted by the 'talks admin'.
+
+=cut
+
+column "accepted" => {
+    data_type          => 'boolean',
+    default_value      => \"false",
+    is_nullable        => 0,
+};
+
+=head2 confirmed
+
+Flag to indicate if the talk has been confirmed by the presentor after
+acceptance
+
+=cut
+
+column "confirmed" => {
+    data_type          => 'boolean',
+    default_value      => \"false",
+    is_nullable        => 0,
+};
+
+=head2 comment
+
+Any comments that the 'talks admin' should know about, not available for the public.
+
+=cut
+
+column "comment" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 room
+
+The room the presentation will be held. Can be changed by the 'talks admin'.
+
+=cut
+
+column "room" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 datetime
+
+Date and Time of the presentation. Can be changed by the 'talks admin'.
+
+=cut
+
+column "datetime" => {
+    data_type          => 'timestamp',
+    is_nullable        => 1,
+};
+
+=head2 track_id
+
+Foreign Key for L<Act::Schema::Result::Track>. Presentations can be part of a 'track' as a serie.
+
+=cut
+
+column "track_id" => {
+    data_type          => 'integer',
+    is_foreign_key     => 1,
+    is_nullable        => 1,
+};
+
+=head2 level
+
+The level this presentation is addressed, usually on off: (1) Any, (2)
+Beginner, (3) Intermediate or (4) Advanced.
+
+=cut
+
+column "level" => {
+    data_type          => 'integer',
+    default_value      => 1,
+    is_nullable        => 1,
+};
+
+=head2 lang
+
+The language this presentayion wil be held in
+
+=cut
+
+column "lang" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
 
 =head1 PRIMARY KEY
 
@@ -170,64 +228,49 @@ __PACKAGE__->add_columns(
 
 =cut
 
-__PACKAGE__->set_primary_key("talk_id");
+primary_key "talk_id";
 
 =head1 RELATIONS
 
 =head2 track
 
-Type: belongs_to
-
-Related object: L<Act::Schema::Result::Track>
+belongs_to related object: L<Act::Schema::Result::Track>
 
 =cut
 
-__PACKAGE__->belongs_to(
-  "track",
-  "Act::Schema::Result::Track",
-  { track_id => "track_id" },
-  {
-    is_deferrable => 0,
-    join_type     => "LEFT",
-    on_delete     => "SET NULL",
-    on_update     => "NO ACTION",
-  },
-);
+belongs_to "track" => "Act::Schema::Result::Track",
+    { track_id => "track_id" },
+    {
+      is_deferrable => 0,
+      join_type     => "LEFT",
+      on_delete     => "SET NULL",
+      on_update     => "NO ACTION",
+    };
 
 =head2 user
 
-Type: belongs_to
-
-Related object: L<Act::Schema::Result::User>
+belongs_to related object: L<Act::Schema::Result::User>
 
 =cut
 
-__PACKAGE__->belongs_to(
-  "user",
-  "Act::Schema::Result::User",
-  { user_id => "user_id" },
-  { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" },
-);
+belongs_to "user" => "Act::Schema::Result::User",
+    { user_id => "user_id" },
+    { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" };
 
 =head2 user_talks
 
-Type: has_many
-
-Related object: L<Act::Schema::Result::UserTalk>
+has_many related object: L<Act::Schema::Result::UserTalk>
 
 =cut
 
-__PACKAGE__->has_many(
-  "user_talks",
-  "Act::Schema::Result::UserTalk",
-  { "foreign.talk_id" => "self.talk_id" },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
+has_many "user_talks" => "Act::Schema::Result::UserTalk",
+    { "foreign.talk_id" => "self.talk_id" },
+    { cascade_copy => 0, cascade_delete => 0 };
 
+=head1 COPYRIGHT
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-11-18 10:52:44
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:b+inVQY0R59GkJ4I4VnvwQ
+(c) 2014 - Th.J. van Hoesel - THEMA-MEDIA NL
 
+=cut
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
