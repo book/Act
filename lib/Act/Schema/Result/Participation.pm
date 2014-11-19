2fc6c279d4897b7a11f735d14c1acf8da895147f
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2014-11-19 10:53:36 +0100

    sugar coated Act::Schema::Result::Participation

--- lib/Act/Schema/Result/Participation.pm
+++ lib/Act/Schema/Result/Participation.pm
@@ -1,106 +1,118 @@
-use utf8;
 package Act::Schema::Result::Participation;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
+use utf8;
+use 'Act::Schema::Candy';
 
 =head1 NAME
 
 Act::Schema::Result::Participation
 
-=cut
+=head1 DESCRIPTION
 
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
+Holds data per user participation
 
 =head1 TABLE: C<participations>
 
 =cut
 
-__PACKAGE__->table("participations");
+table "participations";
 
 =head1 ACCESSORS
 
 =head2 conf_id
 
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 user_id
-
-  data_type: 'integer'
-  is_foreign_key: 1
-  is_nullable: 0
-
-=head2 tshirt_size
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 nb_family
-
-  data_type: 'integer'
-  default_value: 0
-  is_nullable: 1
-
-=head2 datetime
-
-  data_type: 'timestamp'
-  is_nullable: 1
-
-=head2 ip
-
-  data_type: 'text'
-  is_nullable: 1
-
-=head2 attended
-
-  data_type: 'boolean'
-  default_value: false
-  is_nullable: 1
+Cummunity Event ID
 
 =cut
 
-__PACKAGE__->add_columns(
-  "conf_id",
-  { data_type => "text", is_nullable => 0 },
-  "user_id",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
-  "tshirt_size",
-  { data_type => "text", is_nullable => 1 },
-  "nb_family",
-  { data_type => "integer", default_value => 0, is_nullable => 1 },
-  "datetime",
-  { data_type => "timestamp", is_nullable => 1 },
-  "ip",
-  { data_type => "text", is_nullable => 1 },
-  "attended",
-  { data_type => "boolean", default_value => \"false", is_nullable => 1 },
-);
+column "conf_id" => {
+    data_type          => 'text',
+    is_nullable        => 0,
+};
+
+=head2 user_id
+
+Foreign Key to L<Act::Schema::Result::User>
+
+=cut
+
+column "user_id" => {
+    data_type          => 'integer',
+    is_foreign_key     => 1,
+    is_nullable        => 0,
+};
+
+=head2 tshirt_size
+
+Size of the t-shirt, which seems to vary over time
+
+=cut
+
+column "tshirt_size" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 nb_family
+
+Number of family members comming with this participation
+
+=cut
+
+column "nb_family" => {
+    data_type          => 'integer',
+    default_value      => 0,
+    is_nullable        => 1,
+};
+
+=head2 datetime
+
+Timestamp
+
+=cut
+
+column "datetime" => {
+    data_type          => 'timestamp',
+    is_nullable        => 1,
+};
+
+=head2 ip
+
+IP-Address at the time of . . . .
+
+=cut
+
+column "ip" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
+
+=head2 attended
+
+Flag to indicate if the participant actual showed up
+
+=cut
+
+column "attended" => {
+    data_type          => 'boolean',
+    default_value      => \"false",
+    is_nullable        => 1,
+};
+
 
 =head1 RELATIONS
 
 =head2 user
 
-Type: belongs_to
-
-Related object: L<Act::Schema::Result::User>
+belongs_to related object: L<Act::Schema::Result::User>
 
 =cut
 
-__PACKAGE__->belongs_to(
-  "user",
-  "Act::Schema::Result::User",
-  { user_id => "user_id" },
-  { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" },
-);
+belongs_to "user" => "Act::Schema::Result::User",
+    { user_id => "user_id" },
+    { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" };
 
+=head1 COPYRIGHT
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-11-18 10:52:44
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:wxec50FblMmcYJTkO4anHw
+(c) 2014 - Th.J. van Hoesel - THEMA-MEDIA NL
 
-
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
