03841d7510df387ba393a97d84364db5d0216748
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2014-11-21 10:25:49 +0100

    sugar coated Act::Schema::Result::Twostep

--- lib/Act/Schema/Result/Twostep.pm
+++ lib/Act/Schema/Result/Twostep.pm
@@ -1,61 +1,66 @@
-use utf8;
 package Act::Schema::Result::Twostep;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
+use utf8;
+use 'Act::Schema::Candy';
 
 =head1 NAME
 
 Act::Schema::Result::Twostep
 
-=cut
+=head1 DESCRIPTION
 
-use strict;
-use warnings;
-
-use base 'DBIx::Class::Core';
+TODO:
 
 =head1 TABLE: C<twostep>
 
 =cut
 
-__PACKAGE__->table("twostep");
+table "twostep";
 
 =head1 ACCESSORS
 
 =head2 token
 
-  data_type: 'char'
-  is_nullable: 0
-  size: 32
+TODO:
+
+=cut
+column "token" => {
+    data_type          => 'char',
+    is_nullable        => 0,
+    size               => 32,
+};
 
 =head2 email
 
-  data_type: 'text'
-  is_nullable: 0
-
-=head2 datetime
-
-  data_type: 'timestamp'
-  is_nullable: 1
-
-=head2 data
-
-  data_type: 'text'
-  is_nullable: 1
+TODO:
 
 =cut
 
-__PACKAGE__->add_columns(
-  "token",
-  { data_type => "char", is_nullable => 0, size => 32 },
-  "email",
-  { data_type => "text", is_nullable => 0 },
-  "datetime",
-  { data_type => "timestamp", is_nullable => 1 },
-  "data",
-  { data_type => "text", is_nullable => 1 },
-);
+column "email" => {
+    data_type          => 'text',
+    is_nullable        => 0,
+};
+
+=head2 datetime
+
+TODO:
+
+=cut
+
+column "datetime" => {
+    data_type          => 'timestamp',
+    is_nullable        => 1,
+};
+
+=head2 data
+
+TODO:
+
+=cut
+
+column "data" => {
+    data_type          => 'text',
+    is_nullable        => 1,
+};
 
 =head1 PRIMARY KEY
 
@@ -67,12 +72,12 @@ __PACKAGE__->add_columns(
 
 =cut
 
-__PACKAGE__->set_primary_key("token");
+primary_key "token";
 
+=head1 COPYRIGHT
 
-# Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-11-18 10:52:44
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:vXv2ERD+7+V6D99PBZQh4Q
+(c) 2014 - Th.J. van Hoesel - THEMA-MEDIA NL
 
+=cut
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
