27d8cf7f3c35cf32e7270d35261a991cf89a4782
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2016-09-04 00:17:49 +0200

    Sometimes it's still array

--- lib/Act/Middleware/Auth.pm
+++ lib/Act/Middleware/Auth.pm
@@ -111,7 +111,7 @@ sub check_login {
     catch {
         my $env = $req->env;
 
-        my $error = $_;
+        my $error = ref $_ eq 'ARRAY' ? $_->[0] : $_;
         my $full_error = join ' ', map { "[$_]" }
             $env->{SERVER_NAME},
             $req->address,
