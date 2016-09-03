237953b3e4725c3406376f553fe26030f1403983
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2016-09-03 21:49:26 +0200

    Convert handlers to app

--- lib/Act/Dispatcher.pm
+++ lib/Act/Dispatcher.pm
@@ -101,7 +101,7 @@ sub to_app {
 }
 
 sub conference_app {
-    my $static_app = Act::Handler::Static->new;
+    my $static_app = Act::Handler::Static->new->to_app;
     builder {
         enable '+Act::Middleware::Language';
         enable sub {
@@ -158,7 +158,7 @@ sub _handler_app {
         my $subhandler = $1;
     }
     _load($handler);
-    return $handler->new(subhandler => $subhandler);
+    return $handler->new(subhandler => $subhandler)->to_app;
 }
 
 sub _load {
