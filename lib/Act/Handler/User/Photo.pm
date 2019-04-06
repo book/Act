b5c088dcc9818a487a0f9ed23621f5b24166e7d1
Wesley Schwengle <wesley@schwengle.net>
2019-04-06 12:42:58 +0200

    Be able to upload photo's to the filestore
    TODO: Actually show them ;)

--- lib/Act/Handler/User/Photo.pm
+++ lib/Act/Handler/User/Photo.pm
@@ -11,90 +11,119 @@ use Act::Config;
 use Act::Template::HTML;
 use Act::User;
 use Act::Util;
+use Try::Tiny;
 
-sub handler
-{
-    my $error;
-    if (   $Request{args}{update}
-        && $Request{args}{photo}
-        && $Request{r}->upload()
-        && defined(my $fh = $Request{r}->upload()->fh()))
-    {
-        # read uploaded picture
-        my $img = Imager->new();
-        if ($img->read(fh => $fh)) {
+sub _photo_dir_path {
+    if ($Config->general_dir_photos =~ /^\//) {
+        return $Config->general_dir_photos;
+    }
+    return catfile($Config->root, $Config->general_dir_photos);
+}
 
-            # check image format
-            my $format = $img->tags(name => 'i_format');
-            if ($Act::Config::Image_formats{$format}) {
-
-                # see if image needs to be resized
-                my ($w, $h) = map $img->$_, qw(getwidth getheight);
-                my ($wmax, $hmax) = split /\D+/, $Config->general_max_imgsize;
-                if ($w > $wmax || $h > $hmax) {
-                    # image needs resizing
-                    if ($w / $h > $wmax / $hmax) {
-                        $img = $img->scale(xpixels => $wmax);
-                    }
-                    else {
-                        $img = $img->scale(ypixels => $hmax);
-                    }
-                }
-
-                # delete previous photo
-                _delete_photo();
-
-                # compute MD5
-                my $data;
-                $img->write(data => \$data, type => $format)
-                    or die $img->errstr;
-                my $digest = md5_hex($data);
-
-                # store picture
-                my $filename = $digest . $Act::Config::Image_formats{$format};
-                my $pathname = catfile($Config->root,
-                                       $Config->general_dir_photos,
-                                       $filename);
-                $img->write(file => $pathname, type => $format)
-                    or die $img->errstr;
-
-                # update database
-                $Request{user}->update(photo_name => $filename);
-            }
-            else {          # unsupported image format
-                ++$error;
-            }
+sub _resize_photo {
+    my $img = shift;
+    my ($w, $h) = map $img->$_, qw(getwidth getheight);
+    my ($wmax, $hmax) = split /\D+/, $Config->general_max_imgsize;
+    if ($w > $wmax || $h > $hmax) {
+        # image needs resizing
+        if ($w / $h > $wmax / $hmax) {
+            $img = $img->scale(xpixels => $wmax);
         }
-        else {      # image can't be read
-            ++$error;
+        else {
+            $img = $img->scale(ypixels => $hmax);
         }
     }
-    elsif ($Request{args}{delete}) {
-        _delete_photo();
+}
+
+sub _read_photo {
+    my $file = shift;
+    my $img = Imager->new();
+    return $img if $img->read(file => $file);
+    die "Unable to read photo";
+}
+
+sub _assert_format {
+    my $img = shift;
+    my $format = $img->tags(name => 'i_format');
+    if (!exists $Act::Config::Image_formats{$format}) {
+        die "Image format not supported";
+    }
+    return $format;
+}
+
+sub _get_digest {
+    my ($img, $format) = @_;
+    my $data;
+    $img->write(data => \$data, type => $format) or die $img->errstr;
+    return md5_hex($data);
+}
+
+sub _store_img {
+    my ($img, $filename, $format) = @_;
+    my $pathname = catfile(_photo_dir_path(), $filename);
+    $img->write(file => $pathname, type => $format) or die $img->errstr;
+    return $filename;
+}
+
+sub _upload_photo {
+    my $upload = shift;
+
+    my $img    = _read_photo($upload->tempname);
+    my $format = _assert_format($img);
+
+    _resize_photo($img);
+
+    my $digest = _get_digest($img, $format);
+    my $filename = $digest . $Act::Config::Image_formats{$format};
+    my $filename = _store_img($img, $filename, $format);
+
+    _delete_photo();
+
+    $Request{user}->update(photo_name => $filename);
+}
+
+sub handler {
+
+    my $error;
+    my $request = $Request{r};
+    my $params = $request->body_parameters;
+    if ($params->{update}) {
+        try {
+            if (my $uploads = $request->uploads) {
+                die "Multiple uploads found!\n" if keys %$uploads != 1;
+                my ($upload) = values %$uploads;
+                _upload_photo($upload);
+            }
+            else {
+                die "No uploads found!\n";
+            }
+        }
+        catch {
+            $error = $_;
+        };
+    }
+    elsif ($params->{delete}) {
+        delete_photo();
         $Request{user}->update(photo_name => undef);
     }
 
     # display form
     my $template = Act::Template::HTML->new();
     $template->variables(
-      error      => $error,
-      formats    => [ sort keys %Act::Config::Image_formats ],
-      photo_uri  => join('/',
-                        undef,
-                        $Config->general_dir_photos,
-                        $Request{user}{photo_name}
-                    ),
+        error     => $error,
+        formats   => [sort keys %Act::Config::Image_formats],
+        photo_uri => join ('/', undef, $Request{user}{photo_name}),
     );
     $template->process('user/photo');
     return;
+
 }
 
-sub _delete_photo()
-{
-    unlink catfile($Config->root,
-                   $Config->general_dir_photos,
-                   $Request{user}{photo_name})
-        if $Request{user}{photo_name};
+sub _delete_photo() {
+    my $filename = $Request{user}{photo_name};
+    return if !defined $filename or !length $filename;
+    my $pathname = catfile(_photo_dir_path(), $filename);
+    unlink $pathname;
 }
 
 1;
