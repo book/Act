6eb1eb5eaaca6371651934b171467b0e787fd0b5
Harald Jörg <haj@posteo.de>
2019-07-18 10:07:13 +0200

    Make sure that wiki texts passed to diff end in a linefeed

--- lib/Act/Handler/Wiki.pm
+++ lib/Act/Handler/Wiki.pm
@@ -133,8 +133,7 @@ sub wiki_diff
 
         $v{user} = Act::User->new(user_id => $v{metadata}{user_id}[0]);
         $v{last_modified} = DateTime::Format::Pg->parse_datetime($v{last_modified});
-        $v{content} .= "\n" if index($v{content}, "\n") == -1;
-
+        $v{content} =~ s/\n?$/\n/s;
         $versions{$r} = \%v;
     }
 
